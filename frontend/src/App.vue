b2c8b6cd240fe01356aae38df9d6530712fe0810
Vincent Jacques <vincent@vincent-jacques.net>
2024-01-15 10:40:27 +0000

    Install Bootstrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,13 @@
 <template>
-    <h1>Gabby</h1>
+    <div class="container-fluid">
+        <h1>Gabby</h1>
+        <div class="row">
+            <div class="col">
+                <h2>PDF</h2>
+            </div>
+            <div class="col">
+                <h2>Form</h2>
+            </div>
+        </div>
+    </div>
 </template>
