575bb3604fde0d7134922cb31ba23e4ec4b1031f
Vincent Jacques <vincent@vincent-jacques.net>
2024-01-16 08:54:14 +0000

    Extract PdfPicker as a component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,145 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+import PdfPicker from './components/PdfPicker.vue'
+
+const lastSelectedText = ref(null)
+
+function text_selected(text, point) {
+  lastSelectedText.value = text
+}
+</script>
+
 <template>
   <div class="container-fluid">
     <h1>Gabby</h1>
     <div class="row">
       <div class="col">
         <h2>PDF</h2>
-        <div id="canvas-container" style="background-color: blue;">
-          <canvas id="pdf-canvas"></canvas>
-          <canvas id="ui-canvas" @mousedown="mousedown" @mousemove="mousemove" @mouseup="mouseup"></canvas>
-        </div>
+        <PdfPicker src="/test.pdf" :page="1" @text-selected="text_selected" />
       </div>
       <div class="col">
         <h2>Form</h2>
-        <p>Start point: {{ startPoint }}</p>
-        <p>End point: {{ endPoint }}</p>
-        <p>Last selected text: {{ lastSelectedText }}</p>
+        <p>Last selected text:</p><pre>{{ lastSelectedText }}</pre>
       </div>
     </div>
   </div>
 </template>
-
-<script async>
-import { ref } from 'vue'
-
-// @todo Upgrade pdfjs-dist. 2.4.456 is OK but I couldn't get more recent versions to work.
-// (Some versions result in pdfjs being undefined; others juste fail to import.)
-import pdfjs from 'pdfjs-dist/build/pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
-
-var pdf = await pdfjs.getDocument('test.pdf').promise
-var page = await pdf.getPage(1)
-var textContent = await page.getTextContent()
-
-export default {
-  setup() {
-    return {
-      lastSelectedText: ref("None"),
-      startPoint: ref(null),
-      endPoint: ref(null),
-    }
-  },
-  async mounted() {
-    var scale = 1.0
-    var viewport = page.getViewport({scale: scale})
-    console.log(viewport)
-    document.getElementById('canvas-container').style.width = viewport.width + 'px'
-    document.getElementById('canvas-container').style.height = viewport.height + 'px'
-    var pdfCanvas = document.getElementById('pdf-canvas')
-    var pdfContext = pdfCanvas.getContext('2d')
-    pdfCanvas.height = viewport.height
-    pdfCanvas.width = viewport.width
-    var renderContext = {
-      canvasContext: pdfContext,
-      viewport: viewport
-    }
-    await page.render(renderContext).promise
-
-    this.uiCanvas = document.getElementById('ui-canvas')
-    this.uiCanvas.height = viewport.height
-    this.uiCanvas.width = viewport.width
-    this.uiContext = this.uiCanvas.getContext('2d')
-    this.uiContext.setTransform(viewport.transform[0], viewport.transform[1], viewport.transform[2], viewport.transform[3], viewport.transform[4], viewport.transform[5])
-  },
-  methods: {
-    mousedown(e) {
-      this.startPoint = this.uiContext.getTransform().inverse().transformPoint(new DOMPoint(e.layerX, e.layerY))
-    },
-    mousemove(e) {
-      if (this.startPoint !== null) {
-        this.uiContext.save()
-        this.uiContext.setTransform(1, 0, 0, 1, 0, 0)
-        this.uiContext.clearRect(0, 0, this.uiCanvas.width, this.uiCanvas.height)
-        this.uiContext.restore()
-
-        this.endPoint = this.uiContext.getTransform().inverse().transformPoint(new DOMPoint(e.layerX, e.layerY))
-
-        var minX = Math.min(this.startPoint.x, this.endPoint.x)
-        var maxX = Math.max(this.startPoint.x, this.endPoint.x)
-        var minY = Math.min(this.startPoint.y, this.endPoint.y)
-        var maxY = Math.max(this.startPoint.y, this.endPoint.y)
-
-        this.uiContext.beginPath()
-        for (var item of textContent.items) {
-          var itemLeft = item.transform[4]
-          var itemRight = item.transform[4] + item.width
-          var itemTop = item.transform[5]
-          var itemBottom = item.transform[5] + item.height
-          if (
-            itemLeft >= minX && itemRight <= maxX
-            && itemTop >= minY && itemBottom <= maxY
-          ) {
-            this.uiContext.rect(itemLeft, itemTop, item.width, item.height)
-          }
-        }
-        this.uiContext.stroke()
-
-        this.uiContext.beginPath()
-        this.uiContext.rect(minX, minY, maxX - minX, maxY - minY)
-        this.uiContext.stroke()
-      }
-    },
-    mouseup(e) {
-      if (this.startPoint !== null) {
-        this.endPoint = this.uiContext.getTransform().inverse().transformPoint(new DOMPoint(e.layerX, e.layerY))
-
-        var minX = Math.min(this.startPoint.x, this.endPoint.x)
-        var maxX = Math.max(this.startPoint.x, this.endPoint.x)
-        var minY = Math.min(this.startPoint.y, this.endPoint.y)
-        var maxY = Math.max(this.startPoint.y, this.endPoint.y)
-
-        this.lastSelectedText = ""
-        for (var item of textContent.items) {
-          var itemLeft = item.transform[4]
-          var itemRight = item.transform[4] + item.width
-          var itemTop = item.transform[5]
-          var itemBottom = item.transform[5] + item.height
-          if (
-            itemLeft >= minX && itemRight <= maxX
-            && itemTop >= minY && itemBottom <= maxY
-          ) {
-            this.lastSelectedText += ' ' + item.str
-          }
-        }
-
-        this.uiContext.save()
-        this.uiContext.setTransform(1, 0, 0, 1, 0, 0)
-        this.uiContext.clearRect(0, 0, this.uiCanvas.width, this.uiCanvas.height)
-        this.uiContext.restore()
-        this.startPoint = null
-        this.endPoint = null
-      }
-    },
-  },
-};
-</script>
-
-<style>
-#canvas-container {
-  position: relative;
-  float: left;
-  border: 1px black solid;
-}
-#pdf-canvas { position: absolute; left: 0; top: 0; z-index: 0; }
-#ui-canvas { position: absolute; left: 0; top: 0; z-index: 1; }
-</style>
---
+++ frontend/src/components/PdfPicker.vue
@@ -0,0 +1,196 @@
+<script setup>
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  'src': String,
+  'page': Number,
+})
+
+const emit = defineEmits([
+  'text-selected',
+])
+
+// @todo Upgrade pdfjs-dist. 2.4.456 is OK but I couldn't get more recent versions to work.
+// (Some versions result in pdfjs being undefined; others juste fail to import.)
+import pdfjs from 'pdfjs-dist/build/pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`
+
+var textContent = []
+var textSpacingTolerance = 0
+
+var container = null
+var pdfCanvas = null
+var pdfContext = null
+var uiCanvas = null
+var uiContext = null
+
+onMounted(async () => {
+  var pdf = await pdfjs.getDocument(props.src).promise
+  var page = await pdf.getPage(1)
+
+  var scale = 1
+  var viewport = page.getViewport({scale})
+  container.style.width = (viewport.width + 2) + 'px'
+  container.style.height = (viewport.height + 2) + 'px'
+  pdfCanvas.height = viewport.height
+  pdfCanvas.width = viewport.width
+  uiCanvas.height = viewport.height
+  uiCanvas.width = viewport.width
+
+  // Somewhat arbitrary. If the tolerance is too small, then the selected text will contain to many spaces,
+  // not a big deal. If the tolerance is too big, then the selected text could contain too few spaces,
+  // which is a problem.
+  textSpacingTolerance = Math.min(viewport.width, viewport.height) / 1e3
+  // console.log('textSpacingTolerance', textSpacingTolerance)
+
+  pdfContext = pdfCanvas.getContext('2d')
+  uiContext = uiCanvas.getContext('2d')
+  uiContext.setTransform(viewport.transform[0], viewport.transform[1], viewport.transform[2], viewport.transform[3], viewport.transform[4], viewport.transform[5])
+
+  await page.render({
+    canvasContext: pdfContext,
+    viewport,
+  }).promise
+
+  textContent = await page.getTextContent()
+  for (var item of textContent.items) {
+    item.left = item.transform[4]
+    item.right = item.transform[4] + item.width
+    item.bottom = item.transform[5]
+    item.top = item.transform[5] + item.height
+    delete item.transform
+  }
+})
+
+var startPoint = null
+
+function mousedown(event) {
+  startPoint = makeCanvasPoint(event)
+}
+
+function mousemove(event) {
+  if (startPoint !== null) {
+    clearCanvas()
+    
+    const endPoint = makeCanvasPoint(event)
+
+    const minX = Math.min(startPoint.x, endPoint.x)
+    const maxX = Math.max(startPoint.x, endPoint.x)
+    const minY = Math.min(startPoint.y, endPoint.y)
+    const maxY = Math.max(startPoint.y, endPoint.y)
+
+    uiContext.save()
+    uiContext.beginPath()
+    for (var item of textContent.items) {
+      if (
+        item.left >= minX && item.right <= maxX
+        && item.bottom >= minY && item.top <= maxY
+      ) {
+        uiContext.rect(item.left, item.bottom, item.width, item.height)
+      }
+    }
+    uiContext.fillStyle = 'rgba(255, 255, 0, 0.5)'
+    uiContext.strokeStyle = 'rgba(255, 128, 0, 0.5)'
+    uiContext.fill()
+    uiContext.stroke()
+    uiContext.restore()
+
+    uiContext.beginPath()
+    uiContext.rect(minX, minY, maxX - minX, maxY - minY)
+    uiContext.stroke()
+  }
+}
+
+function mouseup(event) {
+  if (startPoint !== null) {
+    clearCanvas()
+
+    const endPoint = makeCanvasPoint(event)
+
+    const minX = Math.min(startPoint.x, endPoint.x)
+    const maxX = Math.max(startPoint.x, endPoint.x)
+    const minY = Math.min(startPoint.y, endPoint.y)
+    const maxY = Math.max(startPoint.y, endPoint.y)
+
+    var lines = ['']
+    var previousItem = null
+    // console.log('New selection')
+    for (var item of textContent.items) {
+      if (
+        item.left >= minX && item.right <= maxX
+        && item.bottom >= minY && item.top <= maxY
+      ) {
+        // console.log(item)
+
+        if (previousItem !== null) {
+          if (Math.abs(previousItem.bottom - item.bottom) > textSpacingTolerance) {
+            lines.push('')
+          } else if(previousItem.right + textSpacingTolerance < item.left) {
+            lines[lines.length - 1] += ' '
+          }
+        }
+
+        lines[lines.length - 1] += item.str
+        previousItem = item
+      }
+    }
+
+    var text = ''
+    for (var line of lines) {
+      line = line.replace(/[ \t]+/g, ' ').trim()
+      if (line !== '') {
+        text += line + '\n'
+      }
+    }
+
+    if (text !== '') {
+      emit('text-selected', text, {clientX: event.clientX, clientY: event.clientY})
+    }
+
+    startPoint = null
+  }
+}
+
+function makeCanvasPoint(event) {
+  const rect = uiCanvas.getBoundingClientRect()
+  return uiContext.getTransform().inverse().transformPoint(
+    new DOMPoint(event.clientX - rect.left, event.clientY - rect.top)
+  )
+}
+
+function clearCanvas() {
+  uiContext.save()
+  uiContext.setTransform(1, 0, 0, 1, 0, 0)
+  uiContext.clearRect(0, 0, uiCanvas.width, uiCanvas.height)
+  uiContext.restore()
+}
+</script>
+
+<template>
+  <p>Page {{ page }} of {{ src }}:</p>
+  <div ref="container" class="container">
+    <canvas ref="pdfCanvas" class="pdf"></canvas>
+    <canvas ref="uiCanvas" class="ui" @mousedown="mousedown" @mousemove="mousemove" @mouseup="mouseup"></canvas>
+  </div>
+</template>
+
+<style scoped>
+div.container {
+  position: relative;
+  border: 1px solid black;
+}
+
+canvas {
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+canvas.pdf {
+  z-index: 0;
+}
+
+canvas.ui {
+  z-index: 1;
+}
+</style>
