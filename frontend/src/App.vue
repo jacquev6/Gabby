9aa2b58386c8d0cd454f7bc7e5ae546d5e03e504
Vincent Jacques <vincent@vincent-jacques.net>
2024-01-17 09:25:27 +0000

    Extract component 'FloatingModal'

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,17 @@
 <script setup>
 import { ref } from 'vue'
-import { useFloating, shift, flip, offset } from '@floating-ui/vue'
-import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import PdfPicker from './components/PdfPicker.vue'
+import FloatingModal from './components/FloatingModal.vue'
 
 const selectedText = ref(null)
-const floatingModal = ref(null)
-const floatingModalDialog = ref(null)
-const floatingModalReference = ref(null)
-const {floatingStyles: floatingModalDialogStyle} = useFloating(floatingModalReference, floatingModalDialog, { placement: 'top', middleware: [offset(-40), flip(), shift()] })
+const showTextSelectionMenu = ref(false)
+const textSelectionMenuReference = ref({x: 0, y: 0})
 
 function text_selected(text, point) {
   selectedText.value = text
-  bootstrap.Modal.getOrCreateInstance(floatingModal.value).show()
-
-  floatingModalReference.value = {
-    getBoundingClientRect() {
-      const { clientX, clientY } = point
-      return {
-        width: 0,
-        height: 0,
-        x: clientX,
-        y: clientY,
-        top: clientY,
-        left: clientX,
-        right: clientX,
-        bottom: clientY,
-      }
-    }
-  }
+  showTextSelectionMenu.value = true
+  textSelectionMenuReference.value = {x: point.clientX, y: point.clientY}
 }
 
 var wording = ref('')
@@ -43,28 +25,23 @@ var directives = ref('')
       <div class="col">
         <h2>PDF</h2>
         <PdfPicker src="/test.pdf" :page="1" @text-selected="text_selected" />
-        <div ref="floatingModal" class="modal">
-          <div ref="floatingModalDialog" class="modal-dialog modal-xl" :style="{...floatingModalDialogStyle, margin: 0}">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h3 class="modal-title">Selected text</h3>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-              </div>
-              <div class="modal-body">
-                <pre>{{ selectedText }}</pre>
-                <p>Add to:</p>
-                <div class="row">
-                  <div class="col">
-                    <p><button class="btn btn-primary" @click="wording += selectedText" data-bs-dismiss="modal">Wording</button></p>
-                  </div>
-                  <div class="col">
-                    <p><button class="btn btn-primary" @click="directives += selectedText" data-bs-dismiss="modal">Directives</button></p>
-                    </div>
-                </div>
-              </div>
+        <FloatingModal
+          v-if="showTextSelectionMenu"
+          title="Selected text"
+          :reference="textSelectionMenuReference"
+          @dismissed="showTextSelectionMenu=false"
+        >
+          <pre>{{ selectedText }}</pre>
+          <p>Add to:</p>
+          <div class="row">
+            <div class="col">
+              <p><button class="btn btn-primary" @click="wording += selectedText; showTextSelectionMenu=false">Wording</button></p>
             </div>
+            <div class="col">
+              <p><button class="btn btn-primary" @click="directives += selectedText; showTextSelectionMenu=false">Directives</button></p>
+              </div>
           </div>
-        </div>
+        </FloatingModal>
       </div>
       <div class="col">
         <h2>Form</h2>
---
+++ frontend/src/components/FloatingModal.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { useFloating, shift, flip, offset } from '@floating-ui/vue'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+const props = defineProps({
+  title: String,
+  reference: Object
+})
+
+const emit = defineEmits([
+  'dismissed',
+])
+
+const modal = ref(null)
+const dialog = ref(null)
+const floatingReference = ref(null)
+const {floatingStyles} = useFloating(floatingReference, dialog, { placement: 'top', middleware: [offset(-40), flip(), shift()] })
+
+onMounted(() => {
+  bootstrap.Modal.getOrCreateInstance(modal.value).show()
+  modal.value.addEventListener('hidden.bs.modal', () => { emit('dismissed') })
+  floatingReference.value = {
+    getBoundingClientRect() {
+      const { x, y } = props.reference
+      return {
+        width: 0, height: 0,
+        x: x, left: x, right: x,
+        y: y, top: y, bottom: y,
+      }
+    }
+  }
+})
+
+onBeforeUnmount(() => { bootstrap.Modal.getOrCreateInstance(modal.value).hide() })
+</script>
+
+<template>
+  <div ref="modal" class="modal">
+    <div ref="dialog" class="modal-dialog modal-xl" :style="{...floatingStyles, margin: 0}">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h3 class="modal-title">{{ title }}</h3>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
