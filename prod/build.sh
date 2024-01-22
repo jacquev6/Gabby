b18a86bdab56811a2115f405b10f350ff5d188e9
Vincent Jacques <vincent@vincent-jacques.net>
2024-01-22 09:50:26 +0000

    Build backend image for production (not actually production-ready)

--- prod/build.sh
+++ prod/build.sh
@@ -7,6 +7,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 
 # Check cleanliness
+test $(git branch --show-current) == main
 git diff --stat --exit-code
 git diff --stat --staged --exit-code
 
@@ -15,7 +16,9 @@ timestamp=$(date +%Y%m%d-%H%M%S)
 
 git tag $timestamp
 docker build .. --file frontend/Dockerfile --build-arg GABBY_VERSION=$timestamp --tag jacquev6/gabby-frontend:$timestamp
+docker build .. --file backend/Dockerfile --build-arg GABBY_VERSION=$timestamp --tag jacquev6/gabby-backend:$timestamp
 
 # Publish
 git push origin --tags
 docker push jacquev6/gabby-frontend:$timestamp
+docker push jacquev6/gabby-backend:$timestamp
