jacquev6/Gabby

commit ddc187cb2bc818498b41f3949abf1654be445b30

Homogenize Bootstrap form components

frontend/src/components/ExerciseForm.vue
@@ -4,6 +4,8 @@ import { ref, watch } from 'vue'
 import FloatingModal from './FloatingModal.vue'
 import OptionalExerciseTextArea from './OptionalExerciseTextArea.vue'
 import RequiredExerciseTextArea from './RequiredExerciseTextArea.vue'
+import BCheckBox from './BootstrapCheckBox.vue'
+import BInput from './BootstrapInput.vue'
 
 
 const props = defineProps({
@@ -22,9 +24,6 @@ const props = defineProps({
 })
 
 const model = defineModel({ type: Object })
-
-const numberFieldId = self.crypto.randomUUID()
-const doStripCheckboxId = self.crypto.randomUUID()
 
 const selectedText = ref(null)
 const doStripExerciceNumber = ref(true)
@@ -61,12 +60,9 @@ defineExpose({
     :reference="textSelectionMenuReference"
     @dismissed="showTextSelectionMenu=false"
   >
-    <div class="form-check">
-      <label class="form-check-label" :for="doStripCheckboxId">{{ $t('doStripExerciceNumber') }}</label>
-      <input class="form-check-input" :id="doStripCheckboxId" type="checkbox" :disabled="!canStripExerciceNumber" v-model="doStripExerciceNumber">
-    </div>
+    <BCheckBox :label="$t('doStripExerciceNumber')" v-model="doStripExerciceNumber" />
+    <RequiredExerciseTextArea :label="$t('selectedText')" v-model="textToAdd" />
 
-    <textarea class="form-control" rows="5" v-model="textToAdd"></textarea>
     <p>{{ $t('addTo') }}</p>
     <button class="btn btn-primary" @click="model.instructions += textToAdd; showTextSelectionMenu=false">{{ $t('instructions') }}</button>
     &nbsp;
@@ -77,10 +73,8 @@ defineExpose({
     <button class="btn btn-primary" @click="model.wording += textToAdd; showTextSelectionMenu=false">{{ $t('wording') }}</button>
   </FloatingModal>
 
-  <div class="mb-3">
-    <label class="form-label" :for="numberFieldId">{{ $t('exerciseNumber') }}</label>
-    <input class="form-control" :id="numberFieldId" type="number" min="1" v-model="model.number" :disabled="fixedNumber"/>
-  </div>
+  <BInput :label="$t('exerciseNumber')" type="number" min="1" v-model="model.number" :disabled="fixedNumber"/>
+
   <RequiredExerciseTextArea :label="$t('instructions')" v-model="model.instructions" />
   <OptionalExerciseTextArea :label="$t('example')" v-model="model.example" />
   <OptionalExerciseTextArea :label="$t('clue')" v-model="model.clue" />

frontend/src/App.vue
@@ -4,6 +4,7 @@ import { ref, reactive, watch } from 'vue'
 import AboutModal from './components/AboutModal.vue'
 import ExerciseForm from './components/ExerciseForm.vue'
 import PdfPicker from './components/PdfPicker.vue'
+import BInput from './components/BootstrapInput.vue'
 
 
 // @todo Handle API failures and slow-downs
@@ -154,18 +155,12 @@ function ellipsis(s) {
       <div class="col-4">
         <div class="row">
           <div class="col">
-            <div class="mb-3">
-              <!-- @todo Display a "loading" animation when loading a new PDF or navigating to another page, until the page is displayed, and the list of existing exercices is retrieved -->
-              <label class="form-label" :for="fileFieldId">{{ $t('inputFile') }}</label>
-              <input class="form-control" :id="fileFieldId" type="file" accept=".pdf" :disabled="mode !== 'list'" @change="(e) => { pdf = e.target.files[0] }" />
-            </div>
+            <!-- @todo Display a "loading" animation when loading a new PDF or navigating to another page, until the page is displayed, and the list of existing exercices is retrieved -->
+            <BInput :label="$t('inputFile')" type="file" accept=".pdf" :disabled="mode !== 'list'" @change="(e) => { pdf = e.target.files[0] }" />
           </div>
           <div class="col-3">
-            <div class="mb-3">
-              <label class="form-label" :for="pageNumberId">{{ $t('pageOver', {count : pdfPagesCount}) }}</label>
-              <!-- @todo Debounce changes in pdfPageNumber: don't start a render for intermediate pages when the user presses "up" a few times in this field -->
-              <input class="form-control" :id="pageNumberId" type="number" :disabled="pdfSha256 === null || mode !== 'list'" v-model="pdfPageNumber" />
-            </div>
+            <!-- @todo Debounce changes in pdfPageNumber: don't start a render for intermediate pages when the user presses "up" a few times in this field -->
+            <BInput :label="$t('pageOver', {count : pdfPagesCount})" type="number" :disabled="pdfSha256 === null || mode !== 'list'" v-model="pdfPageNumber" />
           </div>
         </div>
         <PdfPicker

frontend/src/components/BootstrapCheckBox.vue
@@ -1,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineOptions({
+  // Disable attribute inheritance to apply all fallthrough attributes to the textarea
+  // https://vuejs.org/guide/components/attrs#disabling-attribute-inheritance
+  inheritAttrs: false
+})
+
+const props = defineProps({
+  label: { type: String, required: true },
+})
+
+const model = defineModel({ type: Boolean })
+
+const id = self.crypto.randomUUID()
+</script>
+
+<template>
+  <div class="form-check">
+    <label class="form-check-label" :for="id">{{ label }}</label>
+    <input class="form-check-input" :id="id" type="checkbox" v-model="model" v-bind="$attrs" />
+  </div>
+</template>

frontend/src/components/BootstrapInput.vue
@@ -1,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineOptions({
+  // Disable attribute inheritance to apply all fallthrough attributes to the textarea
+  // https://vuejs.org/guide/components/attrs#disabling-attribute-inheritance
+  inheritAttrs: false
+})
+
+const props = defineProps({
+  label: { type: String, required: true },
+})
+
+const model = defineModel({})
+
+const id = self.crypto.randomUUID()
+</script>
+
+<template>
+  <div class="mb-3">
+    <label class="form-label" :for="id">{{ label }}</label>
+    <input class="form-control" :id="id" v-model="model" v-bind="$attrs"/>
+  </div>
+</template>

commit beb6f758eb7f1547a003332eca2e48e2775a7af4

Fix bug introduced in ddc187cb2bc

frontend/src/components/ExerciseForm.vue
@@ -98,7 +98,7 @@ defineExpose({
     :reference="textSelectionMenuReference"
     @dismissed="showTextSelectionMenu=false"
   >
-    <BCheckBox :label="$t('doStripExerciceNumber')" v-model="doStripExerciceNumber" />
+    <BCheckBox :label="$t('doStripExerciceNumber')" v-model="doStripExerciceNumber" :disabled="!canStripExerciceNumber" />
     <RequiredExerciseTextArea :label="$t('selectedText')" v-model="textToAdd" />
 
     <p>{{ $t('addTo') }}</p>