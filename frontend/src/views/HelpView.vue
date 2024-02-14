e547dc1277a8e513e7c6245461b642d9be5559a9
Vincent Jacques <vincent@vincent-jacques.net>
2024-02-14 09:36:11 +0000

    Make help content reactive (for automatic update during development)

--- frontend/src/views/HelpView.vue
+++ frontend/src/views/HelpView.vue
@@ -1,11 +1,13 @@
 <script setup>
+import { reactive } from 'vue'
+
 import fr from '../content/fr/help.md?raw'
 import en from '../content/en/help.md?raw'
 
 import Markdown from 'vue3-markdown-it';
 
 
-const content = { en, fr }
+const content = reactive({ en, fr })
 </script>
 
 <template>
