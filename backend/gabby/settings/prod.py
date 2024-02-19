192e6eed8d802c907fc8f4ac4878eca59dfb81bf
Vincent Jacques <vincent@vincent-jacques.net>
2024-02-19 10:09:13 +0000

    Run Django checks when building production Docker images

--- backend/gabby/settings/prod.py
+++ backend/gabby/settings/prod.py
@@ -3,10 +3,19 @@ import os
 from .common import *
 
 
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+# Deployment checklist
+# https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SILENCED_SYSTEM_CHECKS = [
+    # @todo(Project management, soon) Understand and consider re-enabling these checks
+    "security.W004",  # You have not set a value for the SECURE_HSTS_SECONDS setting. If your entire site is served only over SSL, you may want to consider setting a value and enabling HTTP Strict Transport Security. Be sure to read the documentation first; enabling HSTS carelessly can cause serious, irreversible problems.
+    "security.W008",  # Your SECURE_SSL_REDIRECT setting is not set to True. Unless your site should be available over both SSL and non-SSL connections, you may want to either set this setting True or configure a load balancer or reverse-proxy server to redirect all connections to HTTPS.
+]
 
 
 SECRET_KEY = os.environ["GABBY_SECRET_KEY"]
+assert len(SECRET_KEY) > 50, "The secret key is too short"  # Required by './manage.py check --deploy'
 DEBUG = False
 
 
