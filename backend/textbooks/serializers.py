b9073ac1add501990f63628f962e4fd1c1df675d
Vincent Jacques <vincent@vincent-jacques.net>
2024-02-22 14:48:29 +0000

    Expose new API

--- backend/textbooks/serializers.py
+++ backend/textbooks/serializers.py
@@ -1,36 +1,93 @@
 from rest_framework_json_api import serializers
 
-from .models import Exercise
+from .models import PdfFile, PdfFileNaming, Textbook, Section, Exercise
+
+
+class PdfFileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PdfFile
+        fields = (
+            "url",
+            "sha256", "bytes_count", "pages_count",
+            "namings", "sections",
+        )
+
+    included_serializers = {
+        "namings": 'textbooks.serializers.PdfFileNamingSerializer',
+        "sections": 'textbooks.serializers.SectionSerializer',
+    }
+
+class PdfFileNamingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PdfFileNaming
+        fields = (
+            "url",
+            "name",
+            "pdf_file",
+        )
+
+    included_serializers = {
+        "pdf_file": 'textbooks.serializers.PdfFileSerializer',
+    }
+
+
+class TextbookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Textbook
+        fields = (
+            "url",
+            "title", "publisher", "year", "isbn",
+            "exercises", "sections",
+        )
+
+    included_serializers = {
+        "exercises": 'textbooks.serializers.ExerciseSerializer',
+        "sections": 'textbooks.serializers.SectionSerializer',
+    }
+
+
+class SectionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Section
+        fields = (
+            "url",
+            "textbook_start_page", "pdf_file_start_page", "pages_count",
+            "textbook", "pdf_file",
+        )
+
+    included_serializers = {
+        "textbook": TextbookSerializer,
+        "pdf_file": PdfFileSerializer,
+    }
 
 
 class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = (
-            "pdf_sha256", "pdf_page",
-            "number",
             "url",
+            "page", "number",
             "instructions", "example", "clue", "wording",
+            "textbook",
         )
+
+    included_serializers = {
+        "textbook": TextbookSerializer,
+    }
 
     # https://stackoverflow.com/questions/22124555 has many answers;
     # I chose the approach described in https://stackoverflow.com/a/50842440/905845
-    def validate_pdf_sha256(self, value):
-        if len(value) != 64:
-            raise serializers.ValidationError("pdfSha256 must be 64 characters long")
-        for c in value:
-            if c not in "0123456789abcdef":
-                raise serializers.ValidationError(f"pdfSha256 must be a hexadecimal string (it contains a '{c}')")
-        if self.instance and value != self.instance.pdf_sha256:
-            raise serializers.ValidationError("pdfSha256 is immutable once set")
+    def validate_textbook(self, value):
+        if self.instance and value != self.instance.textbook:
+            raise serializers.ValidationError("'textbook' is immutable once set.")
         return value
 
-    def validate_pdf_page(self, value):
-        if self.instance and value != self.instance.pdf_page:
-            raise serializers.ValidationError("pdfPage is immutable once set")
+    def validate_page(self, value):
+        if self.instance and value != self.instance.page:
+            raise serializers.ValidationError("'page' is immutable once set.")
         return value
 
     def validate_number(self, value):
         if self.instance and value != self.instance.number:
-            raise serializers.ValidationError("number is immutable once set")
+            raise serializers.ValidationError("'number' is immutable once set.")
         return value
--- backend/textbooks/views.py
+++ backend/textbooks/views.py
@@ -1,13 +1,36 @@
 from rest_framework_json_api.views import ModelViewSet
 
-from .models import Exercise
-from .serializers import ExerciseSerializer
+from .models import PdfFile, PdfFileNaming, Textbook, Section, Exercise
+from .serializers import PdfFileSerializer, PdfFileNamingSerializer, TextbookSerializer, SectionSerializer, ExerciseSerializer
+
+
+class PdfFileViewSet(ModelViewSet):
+    queryset = PdfFile.objects.order_by("sha256")
+    serializer_class = PdfFileSerializer
+
+
+class PdfFileNamingViewSet(ModelViewSet):
+    queryset = PdfFileNaming.objects.order_by("id")
+    serializer_class = PdfFileNamingSerializer
+
+
+class TextbookViewSet(ModelViewSet):
+    queryset = Textbook.objects.order_by("id")
+    serializer_class = TextbookSerializer
+
+
+class SectionViewSet(ModelViewSet):
+    queryset = Section.objects.order_by("id")
+    serializer_class = SectionSerializer
+    filterset_fields = {
+        "pdf_file__sha256": ["exact"],
+    }
 
 
 class ExerciseViewSet(ModelViewSet):
-    queryset = Exercise.objects.all()
+    queryset = Exercise.objects.order_by("id")
     serializer_class = ExerciseSerializer
     filterset_fields = {
-        "pdf_sha256": ["exact"],
-        "pdf_page": ["exact"],
+        "textbook": ["exact"],
+        "page": ["exact"],
     }
