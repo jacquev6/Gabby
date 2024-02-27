cd01eb7b7e50d7a932a8887664d68afd030f1ecd
Vincent Jacques <vincent@vincent-jacques.net>
2024-02-27 16:22:56 +0000

    Remove now-unused Vue route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -25,13 +25,6 @@ const router = createRouter({
       name: 'pdf',
       component: () => import('../views/PdfView.vue')
     },
-    // This route shall be removed when we actually need the Vue Router.
-    // Until then, it's used in end-to-end tests checking the Vue Router is working in all environments.
-    {
-      path: '/other',
-      name: 'other',
-      component: () => import('../views/OtherView.vue')
-    },
   ],
 })
 
