c9e6cc7ca66bbc84aca237d5b924a03d85267084
Vincent Jacques <vincent@vincent-jacques.net>
2024-02-29 05:44:09 +0000

    Todo

--- dev-env/frontend/unit-tests.sh
+++ dev-env/frontend/unit-tests.sh
@@ -10,3 +10,7 @@ docker compose exec \
   frontend \
     npx cypress run \
       --component
+
+
+# @todo(Project management, soon) Ensure that templates have no untranslated content
+# @body(Project management, soon) Ensure that no data-bs-* attributes are used
