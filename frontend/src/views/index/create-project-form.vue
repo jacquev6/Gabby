a4176a8c4240e178fd839cb8bc80b445ab3b5094
Vincent Jacques <vincent@vincent-jacques.net>
2024-03-13 16:38:57 +0000

    Refactor project forms (create and edit)

--- frontend/src/views/index/create-project-form.vue
+++ frontend/src/views/index/create-project-form.vue
@@ -1,38 +1,10 @@
 <script setup>
-import { ref, computed } from 'vue'
-
-import { BBusy, BLabeledInput, BLabeledTextarea, BButton } from '../../components/opinion/bootstrap'
-import { useApiStore } from '../../stores/api'
-
-
-const emit = defineEmits([
-  'created',  // (projectId: string) => void
-])
-
-const api = useApiStore()
-
-const title = ref('')
-const description = ref('')
-const disabled = computed(() => !title.value)
-const creating = ref(false)
-async function create() {
-  creating.value = true
-  const project = await api.client.post(
-    'project',
-    {title: title.value, description: description.value},
-    {textbooks: [], exercises: []},
-  )
-  creating.value = false
-  title.value = ''
-  description.value = ''
-  emit('created', project.id)
-}
+import { BButton } from '../../components/opinion/bootstrap'
+import ProjectForm from '../../components/project-form.vue'
 </script>
 
 <template>
-  <b-busy :busy="creating">
-    <b-labeled-input v-model="title" :label="$t('projectTitle')" />
-    <b-labeled-textarea v-model="description" :label="$t('projectDescription')" />
-    <b-button primary @click="create" :disabled="disabled">{{ $t('createProject' )}}</b-button>
-  </b-busy>
+  <project-form v-slot="{ disabled, create }">
+    <b-button primary :disabled="disabled" @click="create">{{ $t('createProject' )}}</b-button>
+  </project-form>
 </template>
--- frontend/src/views/project/editable-project-header.vue
+++ frontend/src/views/project/editable-project-header.vue
@@ -1,48 +1,27 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
-import { useApiStore } from '../../stores/api'
-import { BButton, BBusy, BLabeledInput, BLabeledTextarea } from '../../components/opinion/bootstrap'
+import ProjectForm from '../../components/project-form.vue'
+import { BButton } from '../../components/opinion/bootstrap'
 
 
 const props = defineProps({
   project: {type: Object, required: true},
 })
 
-const api = useApiStore()
-
 const editing = ref(false)
-const title = ref(null)
-const description = ref(null)
-function startEditing() {
-  title.value = props.project.attributes.title
-  description.value = props.project.attributes.description
-  editing.value = true
-}
-
-// @todo Factorize with create-project-form.vue
-const disabled = computed(() => !title.value)
-const saving = ref(false)
-async function save() {
-  saving.value = true
-  await api.client.patch('project', props.project.id, {title: title.value, description: description.value}, {})
-  saving.value = false
-  editing.value = false
-}
 </script>
 
 <template>
   <template v-if="editing">
     <h1>Project</h1>
-    <b-busy :busy="saving">
-      <b-labeled-input :label="$t('projectTitle')" v-model="title" />
-      <b-labeled-textarea :label="$t('projectDescription')" v-model="description" />
-      <b-button sm secondary @click="editing = false">{{ $t('cancel') }}</b-button>
-      <b-button sm primary @click="save" :disabled="disabled">{{ $t('saveProject') }}</b-button>
-    </b-busy>
+    <project-form :project="project" v-slot="{ disabled, save }" @saved="editing = false">
+      <b-button secondary @click="editing = false">{{ $t('cancel') }}</b-button>
+      <b-button primary @click="save" :disabled="disabled">{{ $t('saveProject') }}</b-button>
+    </project-form>
   </template>
   <template v-else>
-    <h1>{{ $t('projectHeader', {title: project.attributes.title}) }} <b-button sm secondary @click="startEditing">{{ $t('edit') }}</b-button></h1>
+    <h1>{{ $t('projectHeader', {title: project.attributes.title}) }} <b-button sm secondary @click="editing = true">{{ $t('edit') }}</b-button></h1>
     <p>{{ project.attributes.description }}</p>
   </template>
 </template>
