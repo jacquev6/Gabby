5dfe29a45dbb719a510f6218b1107194bc40bfe1
Vincent Jacques <vincent@vincent-jacques.net>
2024-03-14 13:30:13 +0000

    Fix reactivity of exercises list

--- frontend/src/views/project-textbook-page/exercises-list.vue
+++ frontend/src/views/project-textbook-page/exercises-list.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from 'vue'
+import { computedAsync } from '@vueuse/core'
+
 import { BBusy } from '../../components/opinion/bootstrap'
 import { useApiStore } from '../../stores/api'
 
@@ -10,7 +13,12 @@ const props = defineProps({
 
 const api = useApiStore()
 
-const exercises = api.auto.getAll('exercises', {filter: {'textbook': props.textbook.id, 'textbook_page': props.page}})
+const busy = ref(false)
+const exercises = computedAsync(
+  async () => await api.client.getAll('exercises', {filter: {'textbook': props.textbook.id, 'textbook_page': props.page}}),
+  [],
+  busy,
+)
 
 function ellipsis(s) {
   return s.length > 25 ? s.slice(0, 25) + '…' : s
@@ -18,7 +26,7 @@ function ellipsis(s) {
 </script>
 
 <template>
-  <b-busy :busy="exercises.loading">
+  <b-busy :busy>
     <template v-if="exercises.length">
       <p>{{ $t('existingExercises') }}</p>
       <ul>
--- tests/gabby/project-textbook-page-view.cy.js
+++ tests/gabby/project-textbook-page-view.cy.js
@@ -116,12 +116,13 @@ describe('Gabby\'s project\'s textbook page view', () => {
     cy.visit('/project/1/textbook/1/page/7')
     cy.get('button:contains("Delete")').click()
     cy.get('div.busy').should('not.exist')
+    cy.get('p:contains("No exercises yet.")').should('exist')
 
     cy.visit('/project/1/textbook/1/page/7')
     cy.get('div.busy').should('not.exist')
     cy.get('p:contains("No exercises yet.")').should('exist')
 
-    cy.visit('/project/1/textbook/1/page/6')
+    cy.get('button:contains("<")').click()
     cy.get('p:contains("Existing exercises:")').should('exist')
     cy.get('li:contains("3 Complète avec : le, une, …")').should('exist')
     cy.get('li:contains("4 Écris une phrase en respe…")').should('exist')
