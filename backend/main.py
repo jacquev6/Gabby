a650a43f058931bc5cc6f8fe9e3152f982312c2c
Vincent Jacques <vincent@vincent-jacques.net>
2024-03-18 08:13:33 +0000

    Migrate '/reset-for-tests' to the new backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,21 @@
-from fastapi import FastAPI
+import django.conf
+import django.core.management
+import fastapi
 
 
-app = FastAPI()
+django.setup()
+
+
+app = fastapi.FastAPI()
+
+
+# Test-only URL. Not in 'api/...' to avoid accidentally exposing it.
+if django.conf.settings.EXPOSE_RESET_FOR_TESTS_URL:
+    @app.post("/reset-for-tests/yes-im-sure")
+    def reset_for_tests(fixtures: str = None):
+        django.core.management.call_command("flush", interactive=False)
+        django.core.management.call_command("migrate", interactive=False)
+        if fixtures is not None:
+            for fixture in fixtures.split(","):
+                django.core.management.call_command("loaddata", fixture)
+        return {}
--- backend/opinion/urls.py
+++ backend/opinion/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 from opinion_ping.urls import urlpatterns as opinion_ping
-from opinion_reset import urlpatterns as opinion_reset
 
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/', include(opinion_ping)),
-    path('reset-for-tests/', include(opinion_reset)),
 ]
