jacquev6/Gabby

commit ef63b29b2bfc2b0c07edec5701a8e0f70ad039bb

Display adapted exercises on several pagelets

frontend/src/frontend/views/project/textbook/page/AdaptedExercise.vue
@@ -1,0 +1,63 @@
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue'
+
+import { BButton } from '$frontend/components/opinion/bootstrap'
+import Exercise from '$adapted/components/Exercise.vue'
+import { useExercisePagelets } from '$/adapted/composables/exercisePagelets'
+
+
+const props = defineProps<{
+  projectId: string,
+  exerciseId: string,
+  exercise: any,
+}>()
+
+const settings = {
+  tricolorWording: true,
+  wordingParagraphsPerPagelet: 3,
+}
+
+const pageletIndex = ref(0)
+
+const { firstWordingParagraph, lastWordingParagraph, pageletsCount } = useExercisePagelets(
+  computed(() => settings.wordingParagraphsPerPagelet),
+  computed(() => props.exercise.wording.paragraphs.length),
+  pageletIndex,
+)
+
+watch(pageletsCount, () => {
+  if (pageletIndex.value >= pageletsCount.value) {
+    pageletIndex.value = pageletsCount.value - 1
+  }
+})
+
+const exerciseComponent = ref<InstanceType<typeof Exercise> | null>(null)
+</script>
+
+<template>
+  <Exercise
+    ref="exerciseComponent"
+    :projectId="props.projectId"
+    :exerciseId="props.exerciseId"
+    :exercise
+    :firstWordingParagraph
+    :lastWordingParagraph
+    :settings
+    :isPreview="true"
+  />
+  <p>
+    <template v-if="pageletsCount !== 1">
+      <BButton secondary sm @click="pageletIndex -= 1" :disabled="pageletIndex === 0">&lt;&lt;</BButton>
+      <span>{{ pageletIndex + 1 }} / {{ pageletsCount }}</span>
+      <BButton secondary sm @click="pageletIndex += 1" :disabled="pageletIndex === pageletsCount - 1">&gt;&gt;</BButton>
+    </template>
+    <BButton
+      data-cy="erase-responses"
+      secondary sm
+      @click="exerciseComponent?.reinitModels"
+      :disabled="exerciseComponent?.disabledReinitModels"
+    >
+      {{ $t('eraseAnswers') }}
+    </BButton>
+  </p>
+</template>

frontend/src/adapted/RootLayout.vue
@@ -5,6 +5,7 @@ import type { Data } from './types'
 const data = JSON.parse('{{ data }}') as Data
 const settings = {
   tricolorWording: true,
+  wordingParagraphsPerPagelet: 3,
 }
 </script>
 

frontend/src/adapted/components/Exercise.vue
@@ -2,7 +2,6 @@
 import { computed, reactive, watch } from 'vue'
 import { provide } from 'vue'
 
-import { BButton } from '$frontend/components/opinion/bootstrap'
 import type { Settings, Exercise } from '$adapted/types'
 import TricolorSection from './TricolorSection.vue'
 import MonocolorSection from './MonocolorSection.vue'
@@ -13,9 +12,13 @@ const props = withDefaults(defineProps<{
   exerciseId: string,
   exercise: Exercise,
   settings: Settings,
+  firstWordingParagraph?: number | null,
+  lastWordingParagraph?: number | null,
   isPreview?: boolean,
 }>(), {
   isPreview: false,
+  firstWordingParagraph: null,
+  lastWordingParagraph: null,
 })
 
 // @todo Make this key depend on when the exercise (or its adaptation ) was last modified
@@ -69,24 +72,40 @@ watch(
 const id = `adaptedExercise-${ Math.floor(Math.random() * 4000000000) }`
 
 provide('adaptedExerciseTeleportPoint', `#${id}`)
+
+const wordingParagraphs = computed(() => {
+  if (props.firstWordingParagraph === null || props.lastWordingParagraph === null) {
+    return props.exercise.wording.paragraphs
+  } else {
+    return props.exercise.wording.paragraphs.slice(
+      props.firstWordingParagraph,
+      props.lastWordingParagraph,
+    )}
+})
+
+const wordingParagraphIndexOffset = computed(() => {
+  if (props.firstWordingParagraph === null) {
+    return 0
+  } else {
+    return props.firstWordingParagraph
+  }
+})
+
+defineExpose({
+  reinitModels,
+  disabledReinitModels: computed(() => Object.keys(models.wording).length === 0),
+})
 </script>
 
 <template>
   <div :id="id" style="position: relative">
-    <MonocolorSection :section="exercise.instructions" v-model="models.instructions" />
-    <MonocolorSection v-if="exercise.example !== null" :section="exercise.example" v-model="models.example" />
-    <MonocolorSection v-if="exercise.clue !== null" :section="exercise.clue" v-model="models.clue" />
+    <MonocolorSection :paragraphs="exercise.instructions.paragraphs" :paragraphIndexOffset="0" v-model="models.instructions" />
+    <MonocolorSection v-if="exercise.example !== null" :paragraphs="exercise.example.paragraphs" :paragraphIndexOffset="0" v-model="models.example" />
+    <MonocolorSection v-if="exercise.clue !== null" :paragraphs="exercise.clue.paragraphs" :paragraphIndexOffset="0" v-model="models.clue" />
     <hr />
-    <TricolorSection v-if="settings.tricolorWording" :section="exercise.wording" v-model="models.wording" />
-    <MonocolorSection v-else :section="exercise.wording" v-model="models.wording" />
+    <!-- @todo Fix models: they are shared on all pagelets -->
+    <TricolorSection v-if="settings.tricolorWording" :paragraphs="wordingParagraphs" :paragraphIndexOffset="wordingParagraphIndexOffset" v-model="models.wording" />
+    <MonocolorSection v-else :paragraphs="wordingParagraphs" :paragraphIndexOffset="wordingParagraphIndexOffset" v-model="models.wording" />
     <hr />
-    <p><BButton
-      data-cy="erase-responses"
-      secondary sm
-      @click="reinitModels"
-      :disabled="Object.keys(models.wording).length === 0"
-    >
-      {{ $t('eraseAnswers') }}
-    </BButton></p>
   </div>
 </template>

frontend/src/adapted/components/MonocolorSection.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import type { Section } from '$adapted/types'
+import type { Paragraph } from '$adapted/types'
 import MultipleChoicesInput from './MultipleChoicesInput.vue'
 import SelectableText from './SelectableText.vue'
 import SelectedText from './SelectedText.vue'
 
 
 defineProps<{
-  section: Section,
+  paragraphs: Paragraph[],
+  paragraphIndexOffset: number,
 }>()
 
 const models = defineModel<{
@@ -17,10 +18,10 @@ const models = defineModel<{
 </script>
 
 <template>
-  <p v-for="(paragraph, paragraphIndex) in section.paragraphs">
+  <p v-for="(paragraph, paragraphIndex) in paragraphs">
     <template v-for="(sentence, sentenceIndex) in paragraph.sentences">
       <template v-for="(token, tokenIndex) in sentence.tokens">
-        <template v-for="modelKey in [`${paragraphIndex}-${sentenceIndex}-${tokenIndex}`]">
+        <template v-for="modelKey in [`${paragraphIndex + paragraphIndexOffset}-${sentenceIndex}-${tokenIndex}`]">
           <span>
             <template v-if="token.type === 'plainText'">{{ token.text }}</template>
             <template v-else-if="token.type === 'whitespace'"><wbr /> <wbr /></template>

frontend/src/adapted/components/TricolorSection.cy.js
@@ -24,12 +24,11 @@ const color2 = 'rgb(0, 128, 0)'
 const color3 = 'rgb(0, 0, 255)'
 
 const props = {
-  section: {
-    paragraphs: [
-      {sentences: [makeSentence('A'), makeSentence('B'), makeSentence('C')]},
-      {sentences: [makeSentence('D'), makeSentence('E'), makeSentence('F')]},
-    ],
-  },
+  paragraphs: [
+    {sentences: [makeSentence('A'), makeSentence('B'), makeSentence('C')]},
+    {sentences: [makeSentence('D'), makeSentence('E'), makeSentence('F')]},
+  ],
+  paragraphIndexOffset: 0,
   modelValue: [],
 }
 
@@ -78,7 +77,7 @@ describe('TricolorSection', () => {
   it('reacts to text changes', () => {
     cy.mount(TricolorSection, mountOptions)
 
-    cy.vue().then((w) => w.setProps({section: {paragraphs: [{sentences: [
+    cy.vue().then((w) => w.setProps({paragraphs: [{sentences: [
       makeSentence('A', 10),
       makeSentence('B', 10),
       makeSentence('C', 10),
@@ -87,7 +86,7 @@ describe('TricolorSection', () => {
       makeSentence('F', 10),
       makeSentence('G', 10),
       makeSentence('H', 10),
-    ]}]}}))
+    ]}]}))
 
     cy.get('span:contains("AA")').last().should('have.css', 'color', color1)
     cy.get('span:contains("BJ")').last().should('have.css', 'color', color1)
@@ -109,11 +108,10 @@ describe('TricolorSection', () => {
       TricolorSection,
       {
         props: {
-          section: {
-            paragraphs: [
-              {sentences: [makeSentence('A', 10)]},
-            ],
-          },
+          paragraphs: [
+            {sentences: [makeSentence('A', 10)]},
+          ],
+          paragraphIndexOffset: 0,
           modelValue: [],
         },
       },
@@ -135,9 +133,10 @@ describe('TricolorSection', () => {
       TricolorSection,
       {
         props: {
-          section: {paragraphs: [{sentences: [{
+          paragraphs: [{sentences: [{
             tokens: [makeToken('abcdefghijkl'), makeToken(' '), makeToken('mnopqrstuvwxyz')],
-          }]}]},
+          }]}],
+          paragraphIndexOffset: 0,
           modelValue: [],
         },
       },

frontend/src/adapted/components/TricolorSection.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import type { Section } from '$adapted/types'
+import type { Paragraph } from '$adapted/types'
 import TricolorLines from './TricolorLines.vue'
 import MonocolorSection from './MonocolorSection.vue'
 
 
 defineProps<{
-  section: Section,
+  paragraphs: Paragraph[],
+  paragraphIndexOffset: number,
 }>()
 
-const models = defineModel<{
+const model = defineModel<{
   [index: string]: any/* @todo Type */
 }>({
   required: true,
@@ -17,7 +18,7 @@ const models = defineModel<{
 
 <template>
   <TricolorLines>
-    <MonocolorSection :section v-model="models">
+    <MonocolorSection :paragraphs :paragraphIndexOffset v-model="model">
       <template v-slot="{ token, tokenIndex }">
         <slot :token :tokenIndex></slot>
       </template>

frontend/src/adapted/composables/exercisePagelets.ts
@@ -1,0 +1,33 @@
+import { computed, type Ref } from 'vue'
+
+
+export function useExercisePagelets(
+  paragraphsCountPerPagelet: Ref<number | null>,
+  totalParagraphsCount: Ref<number>,
+  pageletIndex: Ref<number>,
+) {
+  return {
+    firstWordingParagraph: computed(() => {
+      if (paragraphsCountPerPagelet.value === null) {
+        return null
+      } else {
+        return pageletIndex.value * paragraphsCountPerPagelet.value
+      }    
+    }),
+    lastWordingParagraph: computed(() => {
+      if (paragraphsCountPerPagelet.value === null) {
+        return null
+      } else {
+        return (pageletIndex.value + 1) * paragraphsCountPerPagelet.value
+      }    
+    }),
+    pageletsCount: computed(() => {
+      if (paragraphsCountPerPagelet.value === null) {
+        return 1
+      } else {
+        console.assert(paragraphsCountPerPagelet.value > 0)
+        return Math.ceil(totalParagraphsCount.value / paragraphsCountPerPagelet.value)
+      }
+    })
+  }
+}

frontend/src/adapted/main.ts
@@ -16,10 +16,16 @@ const router = createRouter({
       component: IndexView,
     },
     {
-      path: '/exercise-:exerciseId',
+      path: '/exercise-:exerciseId/:pageletIndex',
       name: 'exercise',
       component: ExerciseView,
-      props: true,
+      props: (route) => {
+        console.assert(typeof(route.params.pageletIndex) === 'string')
+        return {
+          exerciseId: route.params.exerciseId,
+          pageletIndex: Number.parseInt(route.params.pageletIndex, 10),
+        }
+      },
     },
   ],
 })

commit 5b01810f22e38b1b2c9fe970b907d080123cbbcc

Fix typing errors introduced in ef63b29b2

frontend/src/frontend/views/project/textbook/page/AdaptedExercise.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { ref, computed, watch } from 'vue'
 
+import type { Exercise } from '$adapted/types'
 import { BButton } from '$frontend/components/opinion/bootstrap'
-import Exercise from '$adapted/components/Exercise.vue'
+import ExerciseComponent from '$adapted/components/Exercise.vue'
 import { useExercisePagelets } from '$/adapted/composables/exercisePagelets'
 
 
 const props = defineProps<{
   projectId: string,
   exerciseId: string,
-  exercise: any,
+  exercise: Exercise,
 }>()
 
 const settings = {
@@ -31,11 +32,11 @@ watch(pageletsCount, () => {
   }
 })
 
-const exerciseComponent = ref<InstanceType<typeof Exercise> | null>(null)
+const exerciseComponent = ref<InstanceType<typeof ExerciseComponent> | null>(null)
 </script>
 
 <template>
-  <Exercise
+  <ExerciseComponent
     ref="exerciseComponent"
     :projectId="props.projectId"
     :exerciseId="props.exerciseId"