jacquev6/Gabby

commit 4a7e8e87e3a4e9340e0071b44dcca6d9d28b2101

Authenticate users using their email address

backend/gabby/users/tests.py
@@ -5,7 +5,7 @@ import argon2
 from fastapi import Depends
 import jwt
 
-from .user import User, optional_authenticated_user_dependable, mandatory_authenticated_user_dependable
+from .user import User, UserEmailAddress, optional_authenticated_user_dependable, mandatory_authenticated_user_dependable
 from .. import testing
 
 
@@ -55,7 +55,16 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
 
     def setUp(self):
         super().setUp()
-        self.create_model(User, username="john", clear_text_password="password")
+        self.create_model(
+            UserEmailAddress,
+            user=self.create_model(User, username="john", clear_text_password="password"),
+            address="john@example.com",
+        )
+        self.create_model(
+            UserEmailAddress,
+            user=self.create_model(User, username=None, clear_text_password="anonymous"),
+            address="anonymous@example.com",
+        )
 
     def test_unauthenticated__ok_on_optional(self):
         response = self.api_client.get("http://server/optional-authenticated")
@@ -75,14 +84,14 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
         self.assertEqual(response.status_code, 403, response.json())
         self.assertEqual(response.json(), {"detail": "Incorrect username or password"})
 
-    def test_unexisting_user(self):
+    def test_non_existing_user(self):
         self.expect_rollback()
 
         response = self.api_client.post("http://server/token", data={"username": "bob", "password": "password"})
         self.assertEqual(response.status_code, 403, response.json())
         self.assertEqual(response.json(), {"detail": "Incorrect username or password"})
 
-    def test_password_flow(self):
+    def test_password_flow_using_username(self):
         self.expect_commits_rollbacks(3, 0)
 
         response = self.api_client.post("http://server/token", data={"username": "john", "password": "password"})
@@ -96,6 +105,34 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
         response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200, response.json())
         self.assertEqual(response.json(), {"username": "john"})
+
+    def test_password_flow_using_email_address_for_named_user(self):
+        self.expect_commits_rollbacks(2, 0)
+
+        response = self.api_client.post("http://server/token", data={"username": "john@example.com", "password": "password"})
+        self.assertEqual(response.status_code, 200, response.json())
+        token = response.json()["access_token"]
+
+        response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
+        self.assertEqual(response.status_code, 200, response.json())
+        self.assertEqual(response.json(), {"username": "john"})
+
+    def test_password_flow_using_email_address_for_anonymous_user(self):
+        self.expect_commits_rollbacks(2, 0)
+
+        response = self.api_client.post("http://server/token", data={"username": "anonymous@example.com", "password": "anonymous"})
+        self.assertEqual(response.status_code, 200, response.json())
+        token = response.json()["access_token"]
+
+        response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
+        self.assertEqual(response.status_code, 200, response.json())
+        self.assertEqual(response.json(), {"username": None})
+
+    def test_password_flow_using_email_address_for_non_existing_user(self):
+        self.expect_commits_rollbacks(0, 1)
+
+        response = self.api_client.post("http://server/token", data={"username": "nope@example.com", "password": "password"})
+        self.assertEqual(response.status_code, 403, response.json())
 
     def test_token_expiration(self):
         self.expect_commits_rollbacks(4, 1)

backend/gabby/users/user.py
@@ -24,8 +24,15 @@ class User(OrmBase):
     __tablename__ = "users"
 
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
-    username: orm.Mapped[str] = orm.mapped_column()
+    username: orm.Mapped[str | None] = orm.mapped_column()
     hashed_password: orm.Mapped[str] = orm.mapped_column()
+
+    email_addresses: orm.Mapped[list['UserEmailAddress']] = orm.relationship("UserEmailAddress", back_populates="user")
+
+    __table_args__ = (
+        # NEVER allow '@' in usernames, to avoid confusion with email addresses
+        sql.CheckConstraint("regexp_like(username, '^[-_A-Za-z0-9]+$')", name="check_username"),
+    )
 
     def __init__(self, *, clear_text_password, **kwds):
         super().__init__(hashed_password=self.hash_password(clear_text_password), **kwds)
@@ -47,6 +54,15 @@ class User(OrmBase):
             return True
 
 
+class UserEmailAddress(OrmBase):
+    __tablename__ = "user_email_addresses"
+
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    user_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey("users.id", ondelete="CASCADE"))
+    user: orm.Mapped[User] = orm.relationship("User", back_populates="email_addresses")
+    address: orm.Mapped[str] = orm.mapped_column(unique=True)
+
+
 class UsersResource:
     singular_name = "user"
     plural_name = "users"
@@ -62,7 +78,10 @@ set_wrapper(User, OrmWrapperWithStrIds)
 
 
 def authenticate(session, *, username, clear_text_password):
-    user = session.execute(sql.select(User).filter(User.username == username)).scalar()
+    if "@" in username:
+        user = session.execute(sql.select(User).join(UserEmailAddress).filter(UserEmailAddress.address == username)).scalar()
+    else:
+        user = session.execute(sql.select(User).filter(User.username == username)).scalar()
     if user is None:
         # Do one round of hashing to mitigate timing attacks
         User.hash_password(clear_text_password)

backend/gabby/migrations/versions/2816939acb1c_dev.py
@@ -37,8 +37,9 @@ def upgrade():
     op.create_table(
         "users",
         sa.Column("id", sa.Integer(), nullable=False),
-        sa.Column("username", sa.String(), nullable=False),
+        sa.Column("username", sa.String(), nullable=True),
         sa.Column("hashed_password", sa.String(), nullable=False),
+        sa.CheckConstraint("regexp_like(username, '^[-_A-Za-z0-9]+$')", name="check_username"),
         sa.PrimaryKeyConstraint("id"),
     )
     op.create_table(
@@ -142,6 +143,15 @@ def upgrade():
         sa.UniqueConstraint("id", "project_id"),
     )
     op.create_table(
+        "user_email_addresses",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("user_id", sa.Integer(), nullable=False),
+        sa.Column("address", sa.String(), nullable=False),
+        sa.ForeignKeyConstraint(["user_id"], ["users.id"], ondelete="CASCADE"),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("address"),
+    )
+    op.create_table(
         "exercises",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("project_id", sa.Integer(), nullable=False),
@@ -210,6 +220,7 @@ def downgrade():
     op.drop_table("extraction_events")
     op.drop_table("sections")
     op.drop_table("exercises")
+    op.drop_table("user_email_addresses")
     op.drop_table("textbooks")
     op.drop_table("pings")
     op.drop_table("pdf_file_namings")

backend/gabby/orm_models.py
@@ -7,7 +7,7 @@ from .pdfs import PdfFile, PdfFileNaming
 from .pings import Ping
 from .projects import Project
 from .textbooks import Section, Textbook
-from .users import User
+from .users import User, UserEmailAddress
 
 
 all_models = [
@@ -26,4 +26,5 @@ all_models = [
     SelectThingsAdaptation,
     Textbook,
     User,
+    UserEmailAddress,
 ]

backend/gabby/orm_models.svg
@@ -753,7 +753,45 @@
 <polygon fill="#1e88e5" stroke="#1e88e5" points="1405.98,-938.38 1400.77,-929.15 1399.09,-939.62 1405.98,-938.38"/>
 </a>
 </g>
-<text text-anchor="middle" x="1437.5" y="-1026" font-family="Roboto" font-size="10.00">updated_by</text>
+<text text-anchor="middle" x="1438.5" y="-1026" font-family="Roboto" font-size="10.00">updated_by</text>
+</g>
+<!-- UserEmailAddress -->
+<g id="node16" class="node">
+<title>UserEmailAddress</title>
+<g id="a_node16"><a xlink:href="http://UserEmailAddress_details.html" xlink:title="&lt;TABLE&gt;">
+<polygon fill="#3f51b5" stroke="transparent" points="1306.5,-699 1306.5,-718 1477.5,-718 1477.5,-699 1306.5,-699"/>
+<polygon fill="none" stroke="black" points="1306.5,-699 1306.5,-718 1477.5,-718 1477.5,-699 1306.5,-699"/>
+<text text-anchor="start" x="1337.5" y="-705.4" font-family="Roboto" font-size="12.00" fill="white">UserEmailAddress</text>
+<polygon fill="#bbdefb" stroke="transparent" points="1306.5,-680 1306.5,-699 1359.5,-699 1359.5,-680 1306.5,-680"/>
+<polygon fill="none" stroke="black" points="1306.5,-680 1306.5,-699 1359.5,-699 1359.5,-680 1306.5,-680"/>
+<text text-anchor="start" x="1327.5" y="-686.4" font-family="Roboto" font-size="12.00">id</text>
+<polygon fill="#bbdefb" stroke="transparent" points="1359.5,-680 1359.5,-699 1477.5,-699 1477.5,-680 1359.5,-680"/>
+<polygon fill="none" stroke="black" points="1359.5,-680 1359.5,-699 1477.5,-699 1477.5,-680 1359.5,-680"/>
+<text text-anchor="start" x="1378" y="-686.4" font-family="Roboto" font-size="12.00">INTEGER (PK)</text>
+<polygon fill="#bbdefb" stroke="transparent" points="1306.5,-661 1306.5,-680 1359.5,-680 1359.5,-661 1306.5,-661"/>
+<polygon fill="none" stroke="black" points="1306.5,-661 1306.5,-680 1359.5,-680 1359.5,-661 1306.5,-661"/>
+<text text-anchor="start" x="1311.5" y="-667.4" font-family="Roboto" font-size="12.00">user_id</text>
+<polygon fill="#bbdefb" stroke="transparent" points="1359.5,-661 1359.5,-680 1477.5,-680 1477.5,-661 1359.5,-661"/>
+<polygon fill="none" stroke="black" points="1359.5,-661 1359.5,-680 1477.5,-680 1477.5,-661 1359.5,-661"/>
+<text text-anchor="start" x="1385.5" y="-667.4" font-family="Roboto" font-size="12.00">INTEGER ()</text>
+<polygon fill="#bbdefb" stroke="transparent" points="1306.5,-642 1306.5,-661 1359.5,-661 1359.5,-642 1306.5,-642"/>
+<polygon fill="none" stroke="black" points="1306.5,-642 1306.5,-661 1359.5,-661 1359.5,-642 1306.5,-642"/>
+<text text-anchor="start" x="1309.5" y="-648.4" font-family="Roboto" font-size="12.00">address</text>
+<polygon fill="#bbdefb" stroke="transparent" points="1359.5,-642 1359.5,-661 1477.5,-661 1477.5,-642 1359.5,-642"/>
+<polygon fill="none" stroke="black" points="1359.5,-642 1359.5,-661 1477.5,-661 1477.5,-642 1359.5,-642"/>
+<text text-anchor="start" x="1362.5" y="-648.4" font-family="Roboto" font-size="12.00">VARCHAR (Unique)</text>
+</a>
+</g>
+</g>
+<!-- User&#45;&gt;UserEmailAddress -->
+<g id="edge26" class="edge">
+<title>User&#45;&gt;UserEmailAddress</title>
+<g id="a_edge26"><a xlink:title="Relation between User and UserEmailAddress">
+<path fill="none" stroke="#1e88e5" stroke-dasharray="5,2" d="M1360.95,-844.94C1342.99,-815.6 1326.19,-775.24 1339.5,-740 1340.68,-736.87 1342.11,-733.8 1343.74,-730.81"/>
+<polygon fill="#1e88e5" stroke="#1e88e5" points="1346.86,-732.42 1349.12,-722.07 1340.9,-728.74 1346.86,-732.42"/>
+</a>
+</g>
+<text text-anchor="middle" x="1381" y="-743" font-family="Roboto" font-size="10.00">email_addresses</text>
 </g>
 <!-- SelectThingsAdaptation -->
 <g id="node15" class="node">
@@ -810,6 +848,16 @@
 </a>
 </g>
 <text text-anchor="middle" x="1045" y="-1026" font-family="Roboto" font-size="10.00">exercise</text>
+</g>
+<!-- UserEmailAddress&#45;&gt;User -->
+<g id="edge27" class="edge">
+<title>UserEmailAddress&#45;&gt;User</title>
+<g id="a_edge27"><a xlink:title="Relation between UserEmailAddress and User">
+<path fill="none" stroke="#1e88e5" stroke-dasharray="5,2" d="M1416.29,-722.23C1418.84,-728.03 1421.03,-734.05 1422.5,-740 1430.27,-771.51 1423.02,-807.13 1413.59,-835.3"/>
+<polygon fill="#1e88e5" stroke="#1e88e5" points="1410.21,-834.34 1410.18,-844.93 1416.81,-836.67 1410.21,-834.34"/>
+</a>
+</g>
+<text text-anchor="middle" x="1435.5" y="-743" font-family="Roboto" font-size="10.00">user</text>
 </g>
 </g>
 </svg>

backend/gabby/users/__init__.py
@@ -1,4 +1,4 @@
-from .user import User, UsersResource
+from .user import User, UserEmailAddress, UsersResource
 from .user import authentication_dependable
 from .user import optional_authenticated_user_dependable, OptionalAuthenticatedUserDependent
 from .user import mandatory_authenticated_user_dependable, MandatoryAuthenticatedUserDependent

commit 653a30a8187f05e48a96c7a83572b316102c30ee

Fix huge authentication bug introduced in 4a7e8e87e3a4
'User.username' can be null and is not unique, so it's not a proper key
for the 'users' table.

backend/gabby/users/tests.py
@@ -47,11 +47,11 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
             if user is None:
                 return None
             else:
-                return {"username": user.username}
+                return {"id": user.id, "username": user.username}
 
         @cls.api_app.get("/mandatory-authenticated")
         def get(user: User = Depends(mandatory_authenticated_user_dependable)):
-            return {"username": user.username}
+            return {"id": user.id, "username": user.username}
 
     def setUp(self):
         super().setUp()
@@ -62,8 +62,13 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
         )
         self.create_model(
             UserEmailAddress,
-            user=self.create_model(User, username=None, clear_text_password="anonymous"),
-            address="anonymous@example.com",
+            user=self.create_model(User, username=None, clear_text_password="anonymous-1"),
+            address="anonymous-1@example.com",
+        )
+        self.create_model(
+            UserEmailAddress,
+            user=self.create_model(User, username=None, clear_text_password="anonymous-2"),
+            address="anonymous-2@example.com",
         )
 
     def test_unauthenticated__ok_on_optional(self):
@@ -100,11 +105,11 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
 
         response = self.api_client.get("http://server/optional-authenticated", headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200, response.json())
-        self.assertEqual(response.json(), {"username": "john"})
+        self.assertEqual(response.json(), {"id": 1, "username": "john"})
 
         response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200, response.json())
-        self.assertEqual(response.json(), {"username": "john"})
+        self.assertEqual(response.json(), {"id": 1, "username": "john"})
 
     def test_password_flow_using_email_address_for_named_user(self):
         self.expect_commits_rollbacks(2, 0)
@@ -115,18 +120,29 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
 
         response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200, response.json())
-        self.assertEqual(response.json(), {"username": "john"})
+        self.assertEqual(response.json(), {"id": 1, "username": "john"})
 
-    def test_password_flow_using_email_address_for_anonymous_user(self):
+    def test_password_flow_using_email_address_for_anonymous_1_user(self):
         self.expect_commits_rollbacks(2, 0)
 
-        response = self.api_client.post("http://server/token", data={"username": "anonymous@example.com", "password": "anonymous"})
+        response = self.api_client.post("http://server/token", data={"username": "anonymous-1@example.com", "password": "anonymous-1"})
         self.assertEqual(response.status_code, 200, response.json())
         token = response.json()["access_token"]
 
         response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200, response.json())
-        self.assertEqual(response.json(), {"username": None})
+        self.assertEqual(response.json(), {"id": 2, "username": None})
+
+    def test_password_flow_using_email_address_for_anonymous_2_user(self):
+        self.expect_commits_rollbacks(2, 0)
+
+        response = self.api_client.post("http://server/token", data={"username": "anonymous-2@example.com", "password": "anonymous-2"})
+        self.assertEqual(response.status_code, 200, response.json())
+        token = response.json()["access_token"]
+
+        response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
+        self.assertEqual(response.status_code, 200, response.json())
+        self.assertEqual(response.json(), {"id": 3, "username": None})
 
     def test_password_flow_using_email_address_for_non_existing_user(self):
         self.expect_commits_rollbacks(0, 1)
@@ -143,11 +159,11 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
 
         response = self.api_client.get("http://server/optional-authenticated", headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200, response.json())
-        self.assertEqual(response.json(), {"username": "john"})
+        self.assertEqual(response.json(), {"id": 1, "username": "john"})
 
         response = self.api_client.get("http://server/mandatory-authenticated", headers={"Authorization": f"Bearer {token}"})
         self.assertEqual(response.status_code, 200, response.json())
-        self.assertEqual(response.json(), {"username": "john"})
+        self.assertEqual(response.json(), {"id": 1, "username": "john"})
 
         time.sleep(1)
 
@@ -191,13 +207,13 @@ class AuthenticationApiTestCase(testing.ApiTestCase):
         self.assertLessEqual(valid_until, after + datetime.timedelta(hours=20))
 
         self.assertEqual(token, {
-            "username": "john",
+            "userId": 1,
             "validUntil": valid_until.isoformat(),
         })
         # Try to set 'validUntil' in the token
         tempered_token = jwt.encode(
             {
-                "username": "john",
+                "userId": 1,
                 "validUntil": (valid_until + datetime.timedelta(hours=24)).isoformat(),
             },
             "not-the-secret",

backend/gabby/users/user.py
@@ -111,7 +111,7 @@ def authentication_dependable(
     options.validity = min(options.validity, default_validity)
     valid_until = datetime.datetime.now(tz=datetime.timezone.utc) + options.validity
     token = {
-        "username": user.username,
+        "userId": user.id,
         "validUntil": valid_until.isoformat(),
     }
     return {
@@ -136,7 +136,7 @@ def optional_authenticated_user_dependable(
             return None
         else:
             if datetime.datetime.now(tz=datetime.timezone.utc) < datetime.datetime.fromisoformat(token["validUntil"]):
-                user = session.execute(sql.select(User).filter(User.username == token["username"])).scalar()
+                user = session.get(User, token["userId"])
                 if user is None:
                     # User not found despite having a valid token: user was deleted => silently unauthenticated
                     return None