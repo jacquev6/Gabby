12bcbd98a9dc5b7153c1345f07c3dc34f0b3a91e
Vincent Jacques <vincent@vincent-jacques.net>
2024-06-23 08:05:30 +0000

    Don't export private stuff

--- backend/fastjsonapi/__init__.py
+++ backend/fastjsonapi/__init__.py
@@ -1,3 +1,3 @@
-from .annotations import Annotations, Annotation, Constant, Computed, Secret
+from .annotations import Constant, Computed, Secret
 from .router import make_jsonapi_router
 from .filtering import make_filters
