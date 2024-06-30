cbc4e797262f6b1832537837fc67d7c685946e19
Vincent Jacques <vincent@vincent-jacques.net>
2024-06-30 04:05:27 +0000

    Measure coverage of backend unit tests

--- dev-env/backend/unit-tests.sh
+++ dev-env/backend/unit-tests.sh
@@ -6,6 +6,16 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/."
 
 
+coverage=false
+runner="python"
+if [ $# -gt 0 ] && [ $1 == --coverage ]
+then
+  coverage=true
+  runner="python -m coverage run --branch"
+  shift
+fi
+
+
 defined=$(git grep -e '^    def test' -- ../../backend | wc -l)
 
 imported=$(git grep -e 'from .* import .*ApiTestCase' -- ../../backend/fastjsonapi | wc -l)
@@ -16,4 +26,20 @@ expected=$(expr $defined + $imported + $inherited)
 
 echo "Expecting to run $expected tests"
 
-docker compose exec --env GABBY_UNITTESTING=true backend-shell python -m unittest discover --pattern '*.py' "$@"
+rm -f ../../backend/.coverage
+sudo rm -rf ../backend/unit-tests-coverage
+
+docker compose exec \
+  --env GABBY_UNITTESTING=true \
+  backend-shell \
+    $runner -m unittest discover --pattern '*.py' "$@"
+
+if $coverage
+then
+  docker compose exec \
+    --env GABBY_UNITTESTING=true \
+    backend-shell \
+      python -m coverage html --quiet --dir /app/dev-env/backend/unit-tests-coverage
+fi
+
+rm -f ../../backend/.coverage
