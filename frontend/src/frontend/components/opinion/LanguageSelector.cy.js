0eb92891c74cd0d01c47f9d2403b2fbc9793813b
Vincent Jacques <vincent@vincent-jacques.net>
2024-07-31 07:27:52 +0000

    Make tests more robust

--- frontend/src/frontend/components/opinion/LanguageSelector.cy.js
+++ frontend/src/frontend/components/opinion/LanguageSelector.cy.js
@@ -5,13 +5,13 @@ import { i18n } from '$/locales'
 describe('LanguageSelector', () => {
   it('changes locale', () => {
     cy.mount(LanguageSelector)
-    cy.get('select').should('have.value', 'en')
+    cy.get('select[data-cy="language"]').should('have.value', 'en')
     cy.wrap(i18n).then(p => p.global.t('opinion.ping')).should('eq', 'Ping (in English)')
 
-    cy.get('select').select('fr')
-    cy.get('select').should('have.value', 'fr')
+    cy.get('select[data-cy="language"]').select('fr')
+    cy.get('select[data-cy="language"]').should('have.value', 'fr')
     cy.wrap(i18n).then(p => p.global.t('opinion.ping')).should('eq', 'Ping (en français)')
 
-    cy.get('select').select('en')
+    cy.get('select[data-cy="language"]').select('en')
   })
 })
