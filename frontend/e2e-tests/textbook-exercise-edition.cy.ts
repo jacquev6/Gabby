jacquev6/Gabby

commit 7e474ce39793e16fac3d53f1f3062ef27235789b

Throttle updates of the adapted preview
This was in my mind for a while, and became urgent with the color picker:
it can trigger a lot of changes quickly.

frontend/e2e-tests/textbook-exercise-edition.cy.ts
@@ -138,4 +138,19 @@ describe('Gabby', () => {
     cy.get('label:contains("Adaptation type")').next().should('have.value', 'selectThingsAdaptation')
     cy.get('label:contains("Instructions")').next().should('have.value', 'Instructions!')
   })
+
+  it('throttles updates of the preview', () => {
+    visit('/project-xkopqm/textbook-klxufv/page-7/exercise-vodhqn', {wysiwyg: false})
+
+    cy.get('label:contains("Wording")').next().type('{selectAll}{del}', {delay: 0})
+    notBusy()
+
+    cy.intercept('POST', '/api/parsedExercises').as('parsedExercises')
+    for (let i = 0; i !== 20; i += 1) {
+      cy.get('label:contains("Wording")').next().type(' a', {delay: 0})
+    }
+    notBusy()
+
+    cy.get('@parsedExercises.all').its('length').should('be.lessThan', 15)
+  })
 })

frontend/src/frontend/views/project/textbook/page/ProjectTextbookPageExerciseView.vue
@@ -2,7 +2,7 @@
 import { ref, computed, reactive, watch, nextTick } from 'vue'
 import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
-import { computedAsync } from '@vueuse/core'
+import { computedAsync, watchThrottled } from '@vueuse/core'
 
 import { BButton, BBusy } from '$/frontend/components/opinion/bootstrap'
 import { useApiStore } from '$frontend/stores/api'
@@ -16,7 +16,7 @@ import AdaptedExercise from './AdaptedExercise.vue'
 import ToolsGutter from './ToolsGutter.vue'
 import UndoRedoTool from './UndoRedoTool.vue'
 import { useExerciseCreationHistoryStore } from './ExerciseCreationHistoryStore'
-import { makeModel, assignModelFrom, getParsed, save } from '$frontend/components/ExerciseFieldsForm.vue'
+import { type Model, makeModel, assignModelFrom, getParsed, save } from '$frontend/components/ExerciseFieldsForm.vue'
 import TextSelectionModal from './TextSelectionModal.vue'
 import type { TextualFieldName } from '$/frontend/components/ExerciseFieldsForm.vue'
 import AdaptationDetailsFieldsForm from '$/frontend/components/AdaptationDetailsFieldsForm.vue'
@@ -181,7 +181,21 @@ async function saveThenNext() {
   }
 }
 
-const parsedExerciseLoading = ref(false)
+const throttledModelLateness = ref(0)
+watch(model, () => throttledModelLateness.value++)
+const parsedExerciseWillLoadSoon = computed(() => throttledModelLateness.value > 0)
+const throttledModel = ref<Model>(JSON.parse(JSON.stringify(model)))
+watchThrottled(
+  model,
+  () => {
+    throttledModel.value = JSON.parse(JSON.stringify(model))
+    nextTick(() => {  // To ensure there is no brief moment where 'lateness' is zero but 'isLoading' is not yet true.
+      throttledModelLateness.value = 0
+    })
+  },
+  {throttle: 500},
+)
+const parsedExerciseIsLoading = ref(false)
 // This 'computedAsync' gets cancelled when the user types faster than the server can respond,
 // and only the last response is actually assigned to 'parsedExercise' by VueUse.
 // Thus, 'adaptedExercise' and 'deltas' are -recomputed only when they correspond to the current value of the field.
@@ -189,10 +203,11 @@ const parsedExerciseLoading = ref(false)
 //   it("keeps what's been typed in WYSIWYG fields regardless of the typing speed and server response time"
 // but is arguably fragile. So... don't mess this up, future me!
 const parsedExercise = computedAsync(
-  () => getParsed(model),
+  () => getParsed(throttledModel.value),
   null,
-  parsedExerciseLoading,
+  parsedExerciseIsLoading,
 )
+const parsedExerciseBusy = computed(() => parsedExerciseWillLoadSoon.value || parsedExerciseIsLoading.value)
 
 const adaptedExercise = computed(() => {
   if (parsedExercise.value === null) {
@@ -312,7 +327,7 @@ const toolSlotNames = computed(() => {
           <template #right>
             <div class="h-100 overflow-auto" data-cy="right-col-2">
               <h1>{{ $t('adaptation') }}</h1>
-              <BBusy :busy="parsedExerciseLoading">
+              <BBusy :busy="parsedExerciseBusy">
                 <AdaptedExercise
                   v-if="adaptedExercise !== null"
                   :projectId="projectId"

frontend/src/frontend/views/project/textbook/page/ProjectTextbookPageNewExerciseView.vue
@@ -3,7 +3,7 @@ import { ref, computed, reactive, watch, nextTick } from 'vue'
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
-import { computedAsync } from '@vueuse/core'
+import { computedAsync, watchThrottled } from '@vueuse/core'
 
 import { BButton, BBusy } from '$/frontend/components/opinion/bootstrap'
 import bc from '$frontend/components/breadcrumbs'
@@ -22,7 +22,7 @@ import UndoRedoTool from './UndoRedoTool.vue'
 import type { TextualFieldName } from '$/frontend/components/ExerciseFieldsForm.vue'
 import AdaptationDetailsFieldsForm from '$/frontend/components/AdaptationDetailsFieldsForm.vue'
 import TextSelectionModal from './TextSelectionModal.vue'
-import { makeModel, resetModel, getParsed, create, suggestNextNumber } from '$frontend/components/ExerciseFieldsForm.vue'
+import { type Model, makeModel, resetModel, getParsed, create, suggestNextNumber } from '$frontend/components/ExerciseFieldsForm.vue'
 import type { PDFPageProxy } from 'pdfjs-dist'
 import BasicFormattingTools from './BasicFormattingTools.vue'
 
@@ -156,12 +156,27 @@ function goToPrevious() {
   })
 }
 
-const parsedExerciseLoading = ref(false)
-const parsedExercise = computedAsync(
-  () => getParsed(model),
-  null,
-  parsedExerciseLoading,
+const throttledModelLateness = ref(0)
+watch(model, () => throttledModelLateness.value++)
+const parsedExerciseWillLoadSoon = computed(() => throttledModelLateness.value > 0)
+const throttledModel = ref<Model>(JSON.parse(JSON.stringify(model)))
+watchThrottled(
+  model,
+  () => {
+    throttledModel.value = JSON.parse(JSON.stringify(model))
+    nextTick(() => {  // To ensure there is no brief moment where 'lateness' is zero but 'isLoading' is not yet true.
+      throttledModelLateness.value = 0
+    })
+  },
+  {throttle: 500},
 )
+const parsedExerciseIsLoading = ref(false)
+const parsedExercise = computedAsync(
+  () => getParsed(throttledModel.value),
+  null,
+  parsedExerciseIsLoading,
+)
+const parsedExerciseBusy = computed(() => parsedExerciseWillLoadSoon.value || parsedExerciseIsLoading.value)
 
 const adaptedExercise = computed(() => {
   if (parsedExercise.value === null) {
@@ -283,7 +298,7 @@ const toolSlotNames = computed(() => {
         <template #right>
           <div class="h-100 overflow-auto" data-cy="right-col-2">
             <h1>{{ $t('adaptation') }}</h1>
-            <BBusy :busy="parsedExerciseLoading">
+            <BBusy :busy="parsedExerciseBusy">
               <AdaptedExercise
                 v-if="adaptedExercise !== null"
                 :projectId="projectId"

commit bece44ac7c5240580230ea267b9f10dc45d317f3

Fix race condition introduced in 7e474ce397

frontend/e2e-tests/textbook-exercise-edition.cy.ts
@@ -151,6 +151,6 @@ describe('Gabby', () => {
     }
     notBusy()
 
-    cy.get('@parsedExercises.all').its('length').should('be.lessThan', 15)
+    cy.get('@parsedExercises.all').its('length').should('be.lessThan', 19)
   })
 })

frontend/e2e-tests/user-doc.cy.ts
@@ -122,6 +122,7 @@ describe('Gabby', () => {
     visit('/project-xkopqm/textbook-klxufv/page-6/exercise-wbqloc', {locale: 'fr', pdf: 'test'})
 
     cy.get('label:contains("Énoncé") + div.ql-container > div.ql-editor').type('{selectAll}... vide\n... vident')
+    notBusy()
     cy.screenshot('project-textbook-page-exercise/modify-exercise', {clip: {x: 0, y: 50, width: 575, height: 1000}})
 
     cy.screenshot('project-textbook-page-exercise/tools', {clip: {x: 560, y: 0, width: 210, height: 500}})

frontend/e2e-tests/wysiwyg-edition.cy.ts
@@ -78,19 +78,34 @@ describe('Gabby', () => {
       cy.get('@adaptationType').select(adaptationType)
 
       for (const fieldAlias of ['@instructions', '@wording', '@example', '@clue']) {
-        cy.get(fieldAlias).click().type('plain')
+        cy.get(fieldAlias).click().type('{selectAll}plain', {delay: 0})
         cy.get('@bold').click()
-        cy.get(fieldAlias).type('bold')
+        cy.get(fieldAlias).type('bold', {delay: 0})
         cy.get('@italic').click()
-        cy.get(fieldAlias).type('italic')
+        cy.get(fieldAlias).type('italic', {delay: 0})
 
+        cy.get(fieldAlias).should('contain.text', 'plainbolditalic')
         cy.get('bold-blot:contains("bold")').should('exist')
         cy.get('italic-blot:contains("italic")').should('exist')
+        notBusy()
+        cy.get(fieldAlias).should('contain.text', 'plainbolditalic')
+        cy.get('bold-blot:contains("bold")').should('exist')
+        cy.get('italic-blot:contains("italic")').should('exist')
+        // cy.get('div:has(>h1:contains("Adapted exercise"))').its('length').should('eq', 1)
+        cy.get('div:has(>h1:contains("Adapted exercise")) i:contains("italic")').should('exist')
+        cy.get('div:has(>h1:contains("Adapted exercise")) b:contains("bold")').should('exist')
 
-        cy.get(fieldAlias).type('{selectAll}{del}X')
+        cy.get(fieldAlias).type('{selectAll}X', {delay: 0})
 
+        cy.get(fieldAlias).should('contain.text', 'X')
         cy.get('bold-blot:contains("bold")').should('not.exist')
         cy.get('italic-blot:contains("italic")').should('not.exist')
+        notBusy()
+        cy.get(fieldAlias).should('contain.text', 'X')
+        cy.get('bold-blot:contains("bold")').should('not.exist')
+        cy.get('italic-blot:contains("italic")').should('not.exist')
+        cy.get('div:has(>h1:contains("Adapted exercise")) i:contains("italic")').should('not.exist')
+        cy.get('div:has(>h1:contains("Adapted exercise")) b:contains("bold")').should('not.exist')
       }
     }
   })

frontend/src/frontend/components/ExerciseFieldsForm.vue
@@ -290,6 +290,12 @@ const focusedWysiwygField = computed(() => {
   }
 })
 
+const instructionsDeltas = computed(() => props.deltas === null ? [] : props.deltas.instructions)
+const wordingDeltas = computed(() => props.deltas === null ? [] : props.deltas.wording)
+const exampleDeltas = computed(() => props.deltas === null ? [] : props.deltas.example)
+const clueDeltas = computed(() => props.deltas === null ? [] : props.deltas.clue)
+
+
 const selBlotColors = computed(() => Object.fromEntries(model.value.selectThingsAdaptationOptions.colors.map((color, i) => [`--sel-blot-color-${i + 1}`, color])))
 
 defineExpose({
@@ -312,7 +318,7 @@ defineExpose({
       ref="instructionsEditor"
       :label="$t('exerciseInstructions')"
       :formats="wysiwygFormats[model.adaptationType].instructions"
-      v-model="model.instructions" :delta="deltas === null ? [] : deltas.instructions"
+      v-model="model.instructions" :delta="instructionsDeltas"
     />
     <BLabeledTextarea
       v-else
@@ -325,7 +331,7 @@ defineExpose({
       ref="wordingEditor"
       :label="$t('exerciseWording')"
       :formats="wysiwygFormats[model.adaptationType].wording"
-      v-model="model.wording" :delta="deltas === null ? [] : deltas.wording"
+      v-model="model.wording" :delta="wordingDeltas"
     />
     <BLabeledTextarea
       v-else
@@ -341,7 +347,7 @@ defineExpose({
             ref="exampleEditor"
             :label="$t('exerciseExample')"
             :formats="wysiwygFormats[model.adaptationType].example"
-            :delta="deltas === null ? [] : deltas.example"
+            :delta="exampleDeltas"
             v-model="model.example"
           />
           <OptionalTextarea
@@ -357,7 +363,7 @@ defineExpose({
             ref="clueEditor"
             :label="$t('exerciseClue')"
             :formats="wysiwygFormats[model.adaptationType].clue"
-            :delta="deltas === null ? [] : deltas.clue"
+            :delta="clueDeltas"
             v-model="model.clue"
           />
           <OptionalTextarea

frontend/src/frontend/views/project/textbook/page/ProjectTextbookPageExerciseView.vue
@@ -2,7 +2,6 @@
 import { ref, computed, reactive, watch, nextTick } from 'vue'
 import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
-import { computedAsync, watchThrottled } from '@vueuse/core'
 
 import { BButton, BBusy } from '$/frontend/components/opinion/bootstrap'
 import { useApiStore } from '$frontend/stores/api'
@@ -16,12 +15,12 @@ import AdaptedExercise from './AdaptedExercise.vue'
 import ToolsGutter from './ToolsGutter.vue'
 import UndoRedoTool from './UndoRedoTool.vue'
 import { useExerciseCreationHistoryStore } from './ExerciseCreationHistoryStore'
-import { type Model, makeModel, assignModelFrom, getParsed, save } from '$frontend/components/ExerciseFieldsForm.vue'
+import { makeModel, assignModelFrom, getParsed, save } from '$frontend/components/ExerciseFieldsForm.vue'
 import TextSelectionModal from './TextSelectionModal.vue'
 import type { TextualFieldName } from '$/frontend/components/ExerciseFieldsForm.vue'
 import AdaptationDetailsFieldsForm from '$/frontend/components/AdaptationDetailsFieldsForm.vue'
 import TextPicker from './TextPicker.vue'
-import type { PdfFile } from '$/frontend/stores/api'
+import type { Exists, InCache, ParsedExercise, PdfFile } from '$/frontend/stores/api'
 import type { PDFPageProxy } from 'pdfjs-dist'
 import BasicFormattingTools from './BasicFormattingTools.vue'
 
@@ -181,33 +180,22 @@ async function saveThenNext() {
   }
 }
 
-const throttledModelLateness = ref(0)
-watch(model, () => throttledModelLateness.value++)
-const parsedExerciseWillLoadSoon = computed(() => throttledModelLateness.value > 0)
-const throttledModel = ref<Model>(JSON.parse(JSON.stringify(model)))
-watchThrottled(
-  model,
-  () => {
-    throttledModel.value = JSON.parse(JSON.stringify(model))
-    nextTick(() => {  // To ensure there is no brief moment where 'lateness' is zero but 'isLoading' is not yet true.
-      throttledModelLateness.value = 0
-    })
-  },
-  {throttle: 500},
-)
+const parsedExercise = ref<ParsedExercise & InCache & Exists | null>(null)
 const parsedExerciseIsLoading = ref(false)
-// This 'computedAsync' gets cancelled when the user types faster than the server can respond,
-// and only the last response is actually assigned to 'parsedExercise' by VueUse.
-// Thus, 'adaptedExercise' and 'deltas' are -recomputed only when they correspond to the current value of the field.
-// This is the expected behavior, but is very difficult to test. An attempt to test it has been made in
-//   it("keeps what's been typed in WYSIWYG fields regardless of the typing speed and server response time"
-// but is arguably fragile. So... don't mess this up, future me!
-const parsedExercise = computedAsync(
-  () => getParsed(throttledModel.value),
-  null,
-  parsedExerciseIsLoading,
-)
-const parsedExerciseBusy = computed(() => parsedExerciseWillLoadSoon.value || parsedExerciseIsLoading.value)
+const parsedExerciseNeedsLoading = ref(true)
+watch(model, () => { parsedExerciseNeedsLoading.value = true })
+watch(parsedExerciseNeedsLoading, async () => {
+  while (parsedExerciseNeedsLoading.value) {
+    const modelBefore = JSON.stringify(model)
+    parsedExerciseIsLoading.value = true
+    const parsed = await getParsed(model)
+    if (JSON.stringify(model) === modelBefore) {
+      parsedExercise.value = parsed
+      parsedExerciseIsLoading.value = false
+      parsedExerciseNeedsLoading.value = false
+    }
+  }
+}, {immediate: true})
 
 const adaptedExercise = computed(() => {
   if (parsedExercise.value === null) {
@@ -327,7 +315,7 @@ const toolSlotNames = computed(() => {
           <template #right>
             <div class="h-100 overflow-auto" data-cy="right-col-2">
               <h1>{{ $t('adaptation') }}</h1>
-              <BBusy :busy="parsedExerciseBusy">
+              <BBusy :busy="parsedExerciseIsLoading">
                 <AdaptedExercise
                   v-if="adaptedExercise !== null"
                   :projectId="projectId"

frontend/src/frontend/views/project/textbook/page/ProjectTextbookPageNewExerciseView.vue
@@ -3,7 +3,6 @@ import { ref, computed, reactive, watch, nextTick } from 'vue'
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
-import { computedAsync, watchThrottled } from '@vueuse/core'
 
 import { BButton, BBusy } from '$/frontend/components/opinion/bootstrap'
 import bc from '$frontend/components/breadcrumbs'
@@ -15,14 +14,14 @@ import TwoResizableColumns from '$frontend/components/TwoResizableColumns.vue'
 import ExerciseFieldsForm from '$/frontend/components/ExerciseFieldsForm.vue'
 import { useExerciseCreationHistoryStore } from './ExerciseCreationHistoryStore'
 import { useApiStore } from '$/frontend/stores/api'
-import type { PdfFile } from '$/frontend/stores/api'
+import type { Exists, InCache, ParsedExercise, PdfFile } from '$/frontend/stores/api'
 import AdaptedExercise from './AdaptedExercise.vue'
 import ToolsGutter from './ToolsGutter.vue'
 import UndoRedoTool from './UndoRedoTool.vue'
 import type { TextualFieldName } from '$/frontend/components/ExerciseFieldsForm.vue'
 import AdaptationDetailsFieldsForm from '$/frontend/components/AdaptationDetailsFieldsForm.vue'
 import TextSelectionModal from './TextSelectionModal.vue'
-import { type Model, makeModel, resetModel, getParsed, create, suggestNextNumber } from '$frontend/components/ExerciseFieldsForm.vue'
+import { makeModel, resetModel, getParsed, create, suggestNextNumber } from '$frontend/components/ExerciseFieldsForm.vue'
 import type { PDFPageProxy } from 'pdfjs-dist'
 import BasicFormattingTools from './BasicFormattingTools.vue'
 
@@ -156,27 +155,22 @@ function goToPrevious() {
   })
 }
 
-const throttledModelLateness = ref(0)
-watch(model, () => throttledModelLateness.value++)
-const parsedExerciseWillLoadSoon = computed(() => throttledModelLateness.value > 0)
-const throttledModel = ref<Model>(JSON.parse(JSON.stringify(model)))
-watchThrottled(
-  model,
-  () => {
-    throttledModel.value = JSON.parse(JSON.stringify(model))
-    nextTick(() => {  // To ensure there is no brief moment where 'lateness' is zero but 'isLoading' is not yet true.
-      throttledModelLateness.value = 0
-    })
-  },
-  {throttle: 500},
-)
+const parsedExercise = ref<ParsedExercise & InCache & Exists | null>(null)
 const parsedExerciseIsLoading = ref(false)
-const parsedExercise = computedAsync(
-  () => getParsed(throttledModel.value),
-  null,
-  parsedExerciseIsLoading,
-)
-const parsedExerciseBusy = computed(() => parsedExerciseWillLoadSoon.value || parsedExerciseIsLoading.value)
+const parsedExerciseNeedsLoading = ref(true)
+watch(model, () => { parsedExerciseNeedsLoading.value = true })
+watch(parsedExerciseNeedsLoading, async () => {
+  while (parsedExerciseNeedsLoading.value) {
+    const modelBefore = JSON.stringify(model)
+    parsedExerciseIsLoading.value = true
+    const parsed = await getParsed(model)
+    if (JSON.stringify(model) === modelBefore) {
+      parsedExercise.value = parsed
+      parsedExerciseIsLoading.value = false
+      parsedExerciseNeedsLoading.value = false
+    }
+  }
+}, {immediate: true})
 
 const adaptedExercise = computed(() => {
   if (parsedExercise.value === null) {
@@ -298,7 +292,7 @@ const toolSlotNames = computed(() => {
         <template #right>
           <div class="h-100 overflow-auto" data-cy="right-col-2">
             <h1>{{ $t('adaptation') }}</h1>
-            <BBusy :busy="parsedExerciseBusy">
+            <BBusy :busy="parsedExerciseIsLoading">
               <AdaptedExercise
                 v-if="adaptedExercise !== null"
                 :projectId="projectId"