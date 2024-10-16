3b0da28b4247693fe09ecb1f8eb938d52138a8ff
Vincent Jacques <vincent@vincent-jacques.net>
2024-10-16 12:30:46 +0200

    Fix end-to-end tests using a more reliable workaround

--- frontend/e2e-tests/authentication-and-password-recovery.cy.ts
+++ frontend/e2e-tests/authentication-and-password-recovery.cy.ts
@@ -9,14 +9,13 @@ describe('Gabby\'s authentication system', () => {
 
   it('logs in and out', () => {
     visit('/')
+    cy.wait(1000)  // This is a workaround for fields being reset after we type in them. I can't figure out why this happens.
 
     cy.get('h1:contains("Existing projects")').should('not.exist')
     cy.get('h1:contains("Please log in")').should('exist')
 
-    cy.get('[name=username]').type('jacquev6+gabby-dev-alice@gmail.com', {delay: 0})  // This often leaves the field with just the few characters, e.g. 'adm'. I can't figure out why; probably some race condition.
+    cy.get('[name=username]').type('jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
     cy.get('[name=password]').type('alice-password', {delay: 0})
-    cy.get('[name=username]').type('{selectall}jacquev6+gabby-dev-alice@gmail.com', {delay: 0})  // This is a workaround for the above issue.
-    cy.get('[name=username]').should('have.value', 'jacquev6+gabby-dev-alice@gmail.com')
     cy.get('button:contains("Log in")').click()
 
     notBusy()
@@ -32,10 +31,10 @@ describe('Gabby\'s authentication system', () => {
 
   it('warns about wrong credentials', () => {
     visit('/')
+    cy.wait(1000)  // Workaround, described above.
 
     cy.get('[name=username]').type('jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
     cy.get('[name=password]').type('not-alice-password', {delay: 0})
-    cy.get('[name=username]').type('{selectall}jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
     cy.get('button:contains("Log in")').click()
 
     notBusy()
@@ -47,6 +46,7 @@ describe('Gabby\'s authentication system', () => {
 
   it('does not request a password recovery link', () => {
     visit('/')
+    cy.wait(1000)  // Workaround, described above.
 
     cy.get('h1:contains("Please log in")').should('exist')
 
@@ -63,6 +63,7 @@ describe('Gabby\'s authentication system', () => {
 
   it('requests a password recovery link', () => {
     visit('/')
+    cy.wait(1000)  // Workaround, described above.
 
     cy.get('h1:contains("Please log in")').should('exist')
 
@@ -72,8 +73,6 @@ describe('Gabby\'s authentication system', () => {
     cy.get('h1:contains("Lost password")').should('exist')
 
     cy.get('[name=username]').type('jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
-    cy.get('[name=username]').type('{selectAll}jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
-    cy.get('[name=username]').should('have.value', 'jacquev6+gabby-dev-alice@gmail.com')
 
     cy.intercept('POST', '/api/recoveryEmailRequests', {
       statusCode: 201,
@@ -101,11 +100,9 @@ describe('Gabby\'s authentication system', () => {
   // Get a token (hack for testing, not the usual flow)
   function getTokenThen(f: (token: string) => void) {
     visit('/')
+    cy.wait(1000)  // Workaround, described above.
     cy.get('[name=username]').type('jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
     cy.get('[name=password]').type('alice-password', {delay: 0})
-    cy.get('[name=username]').type('{selectall}jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
-    cy.get('[name=username]').should('have.value', 'jacquev6+gabby-dev-alice@gmail.com')
-    cy.get('[name=password]').should('have.value', 'alice-password')
     cy.intercept('POST', '/api/token').as('tokenRequest')
     cy.get('button:contains("Log in")').click()
     notBusy()
@@ -116,13 +113,11 @@ describe('Gabby\'s authentication system', () => {
   it('resets a password', () => {
     getTokenThen((token) => {
       visit(`/reset-password/jacquev6+gabby-dev-alice@gmail.com/${token}`)
+      cy.wait(1000)  // Workaround, described above.
 
       cy.get('h1:contains("Reset password")').should('exist')
       cy.get('label:contains("New password")').first().next().type('new-alice-password', {delay: 0})
       cy.get('label:contains("New password")').last().next().type('new-alice-password', {delay: 0})
-      cy.get('label:contains("New password")').first().next().type('{selectAll}new-alice-password', {delay: 0})
-      cy.get('label:contains("New password")').first().next().should('have.value', 'new-alice-password')
-      cy.get('label:contains("New password")').last().next().should('have.value', 'new-alice-password')
       cy.get('button:contains("Reset password")').click()
 
       notBusy()
@@ -135,8 +130,6 @@ describe('Gabby\'s authentication system', () => {
 
       cy.get('[name=username]').type('jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
       cy.get('[name=password]').type('alice-password', {delay: 0})
-      cy.get('[name=username]').type('{selectall}jacquev6+gabby-dev-alice@gmail.com', {delay: 0})
-      cy.get('[name=password]').type('{selectall}alice-password', {delay: 0})
       cy.get('button:contains("Log in")').click()
 
       notBusy()
@@ -153,22 +146,18 @@ describe('Gabby\'s authentication system', () => {
 
   it('warns about passwords being different', () => {
     visit('/reset-password/jacquev6+gabby-dev-alice@gmail.com/unused-json-web-token')
+    cy.wait(1000)  // Workaround, described above.
 
     cy.get('button:contains("Reset password")').should('be.disabled')
     cy.get('p:contains("Passwords are different.")').should('not.exist')
 
     cy.get('label:contains("New password")').first().next().type('new-alice-password', {delay: 0})
     cy.get('label:contains("New password")').last().next().type('new-alice', {delay: 0})
-    cy.get('label:contains("New password")').first().next().type('{selectAll}new-alice-password', {delay: 0})
-    cy.get('label:contains("New password")').first().next().should('have.value', 'new-alice-password')
-    cy.get('label:contains("New password")').last().next().should('have.value', 'new-alice')
 
     cy.get('button:contains("Reset password")').should('be.disabled')
     cy.get('p:contains("Passwords are different.")').should('exist')
 
     cy.get('label:contains("New password")').last().next().type('-password', {delay: 0})
-    cy.get('label:contains("New password")').first().next().should('have.value', 'new-alice-password')
-    cy.get('label:contains("New password")').last().next().should('have.value', 'new-alice-password')
 
     cy.get('button:contains("Reset password")').should('be.enabled')
     cy.get('p:contains("Passwords are different.")').should('not.exist')
