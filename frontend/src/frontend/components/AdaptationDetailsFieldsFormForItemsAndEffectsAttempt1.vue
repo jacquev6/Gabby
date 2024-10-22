jacquev6/Gabby

commit ac29ab4fc3f5568d4625639beab0467653f442a9

Implement 'FloatingColorPicker' in terms of 'ContextMenu'...
this fixes a few bugs:
- one test in 'demo.cy.ts' was broken
- the color picker was confined in the "Tools" column,
which was hiding its right end

frontend/src/frontend/components/FloatingColorPicker.vue
@@ -5,49 +5,26 @@ import '@cyhnkckali/vue3-color-picker/dist/style.css'
 import { computed, ref } from 'vue'
 
 import { BButton } from './opinion/bootstrap'
+import ContextMenu from './ContextMenu.vue'
 
 
 const props = defineProps<{
   default: string
+  backdropCovers1: string
+  backdropCovers2: string
 }>()
 
 const model = defineModel<string>({required: true})
 const initialValue = ref('')
 
-const inDom = ref(false)
-const floatingReference = ref<HTMLElement | null>(null)
-const floatingContainer = ref<HTMLElement | null>(null)
-const floatingArrow = ref<HTMLElement | null>(null)
+
+const contextMenu = ref<InstanceType<typeof ContextMenu> | null>(null)
 function show(ref: HTMLElement) {
-  inDom.value = true
-  floatingReference.value = ref
+  console.assert(contextMenu.value !== null)
+
   initialValue.value = model.value
+  contextMenu.value.show(ref)
 }
-
-// @todo Use 'ContextMenu' component
-const {floatingStyles, middlewareData} = useFloating(
-  floatingReference,
-  floatingContainer,
-  {
-    middleware: [
-      shift(),
-      arrow({element: floatingArrow}),
-    ],
-    placement: 'bottom',
-  },
-)
-
-const floatingArrowStyles = computed(() => {
-  const arrow = middlewareData.value.arrow
-  if (arrow === undefined) {
-    return {}
-  } else {
-    console.assert(arrow.x !== undefined)
-    return {
-      left: `${arrow.x}px`,
-    }
-  }
-})
 
 function reset() {
   model.value = props.default
@@ -60,45 +37,29 @@ function rollback() {
 }
 
 function commit() {
-  inDom.value = false
+  console.assert(contextMenu.value !== null)
+  contextMenu.value.hide()
 }
 
 defineExpose({show})
 </script>
 
 <template>
-  <template v-if="inDom">
-    <div
-      class="floating-backdrop"
-      @click="rollback" @contextmenu.prevent="rollback"
-    ></div>
-    <div
-      ref="floatingContainer"
-      class="floating-container"
-      :style="floatingStyles"
-    >
-      <div
-        ref="floatingArrow"
-        class="floating-arrow"
-        :style="floatingArrowStyles"
-      ></div>
-      <div class="floating-content">
-        <ColorPicker
-          v-model="model"
-          mode="solid"
-          type="HEX"
-          :showColorList="false"
-          :showEyeDrop="false"
-          :showAlpha="false"
-          :showInputMenu="false"
-          :showInputSet="false"
-          :showPickerMode="false"
-        />
-        <BButton sm secondary @click="reset">{{ $t('resetColorToDefault') }} <span class="default-color" :style="{background: props.default}"></span></BButton>
-        <BButton sm primary @click="commit">OK</BButton>
-      </div>
-    </div>
-  </template>
+  <ContextMenu ref="contextMenu" :backdropCovers1 :backdropCovers2>
+    <ColorPicker
+      v-model="model"
+      mode="solid"
+      type="HEX"
+      :showColorList="false"
+      :showEyeDrop="false"
+      :showAlpha="false"
+      :showInputMenu="false"
+      :showInputSet="false"
+      :showPickerMode="false"
+    />
+    <BButton sm secondary @click="reset">{{ $t('resetColorToDefault') }} <span class="default-color" :style="{background: props.default}"></span></BButton>
+    <BButton sm primary @click="commit">OK</BButton>
+  </ContextMenu>
 </template>
 
 <style scoped>
@@ -106,36 +67,5 @@ span.default-color {
   display: inline flow-root;
   width: 1.25em;
   height: 1.25em;
-}
-
-div.floating-backdrop {
-  position: fixed;
-  z-index: 10;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0);
-}
-
-div.floating-container {
-  z-index: 11;
-}
-
-div.floating-content {
-  border: 1px solid black;
-  background: white;
-  position: relative;
-  top: 10px;
-}
-
-div.floating-arrow {
-  position: absolute;
-  /* CSS Triangle trick: https://css-tricks.com/snippets/css/css-triangle/ */
-  width: 0;
-  height: 0;
-  border-left: 0.75em solid transparent;
-  border-right: 0.75em solid transparent;
-  border-bottom: 0.75em solid black;
 }
 </style>

frontend/src/frontend/components/AdaptationDetailsFieldsForm.vue
@@ -308,6 +308,8 @@ const colorsCount = computed({
         ref="colorPickers"
         v-model="colors[i - 1].value"
         :default="defaultColors[i - 1]"
+        backdropCovers1="#left-col-2"
+        backdropCovers2="#gutter-2"
       />
       <div class="mb-3">
         <label class="form-label" for="blah">{{ $t('usableColors' )}}</label>

commit a64cc6c9d0d8a2bfedfc54584a5230388cd0b4cd

Fix Typescript errors introduced in ac29ab4fc3

frontend/src/frontend/components/AdaptationDetailsFieldsFormForItemsAndEffectsAttempt1.vue
@@ -113,6 +113,8 @@ const itemsKind = computed({
     ref="colorPickers"
     v-model="colorsProxyForSelectableEffect[i - 1].value"
     :default="defaultColorsForSelectableEffect[i - 1]"
+    backdropCovers1="#left-col-2"
+    backdropCovers2="#gutter-2"
   />
 
   <div class="mb-3">

frontend/src/frontend/components/FloatingColorPicker.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { useFloating, arrow, shift } from '@floating-ui/vue'
 import { Vue3ColorPicker as ColorPicker } from '@cyhnkckali/vue3-color-picker'
 import '@cyhnkckali/vue3-color-picker/dist/style.css'
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 
 import { BButton } from './opinion/bootstrap'
 import ContextMenu from './ContextMenu.vue'
@@ -28,11 +27,6 @@ function show(ref: HTMLElement) {
 
 function reset() {
   model.value = props.default
-  commit()
-}
-
-function rollback() {
-  model.value = initialValue.value
   commit()
 }
 