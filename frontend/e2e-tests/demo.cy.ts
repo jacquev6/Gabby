d6725d142a9d25e76f043bb50628192ad4dce609
Vincent Jacques <vincent@vincent-jacques.net>
2024-10-30 12:30:11 +0100

    Write demo test for new workflow for "Multiple Choices (in wording)"
    (should have been done in branch 'topics/wysiwyg-multiple-choices')

--- frontend/e2e-tests/demo.cy.ts
+++ frontend/e2e-tests/demo.cy.ts
@@ -39,6 +39,7 @@ describe('Gabby', () => {
     cy.get('label:contains("Number") + input').as('number')
     cy.get('label:contains("Adaptation type") + select').as('adaptationType')
     cy.get('label:contains("Instructions") + div.ql-container div.ql-editor').as('instructions')
+    cy.get('label:contains("Wording") + div.ql-container div.ql-editor').as('wording')
   }
 
   function selectRange(startNode: Node, startOffset: number, endNode: Node, endOffset: number) {
@@ -56,12 +57,15 @@ describe('Gabby', () => {
     })
   }
 
-  function screenshot(testName: string, screenshotName: string) {
-    cy.window().then(window => {
-      var sel = window.getSelection()
-      console.assert(sel !== null)
-      sel.removeAllRanges()
-    })
+  function screenshot(testName: string, screenshotName: string, options: {clearSel: boolean} = {clearSel: true}) {
+    if (options.clearSel) {
+      cy.window().then(window => {
+        var sel = window.getSelection()
+        console.assert(sel !== null)
+        sel.removeAllRanges()
+      })
+    }
+    notBusy()
     cy.screenshot(`${testName}--${screenshotName}`)
   }
 
@@ -218,6 +222,47 @@ describe('Gabby', () => {
       screenshot('multiple-choices-in-instructions', 'export-2')
       cy.get('span.choice0').click()
       screenshot('multiple-choices-in-instructions', 'export-3')
+    })
+  })
+
+  it('creates a "Multiple choices (in wording)" exercise', () => {
+    cy.viewport(1200, 850)
+    visit('/project-xkopqm/textbook-klxufv/page-6/new-exercise', {pdf: 'demo'})
+    setupAliases()
+
+    cy.get('@number').type('1')
+
+    cy.get('@adaptationType').select('multiple-choices-in-wording').blur()
+
+    traceRectangle('@canvas', 8, 5, 54, 9)
+    cy.get('button:contains("Instructions")').click()
+    notBusy()
+    traceRectangle('@canvas', 7, 9.25, 49, 15)
+    cy.get('button:contains("Wording")').click()
+    notBusy()
+
+    cy.get('button:contains("Multiple choices")').click()
+    screenshot('multiple-choices-in-wording', 'edit-1')
+    cy.get('@wording').find('p').then($el => {
+      const node = $el[0].firstChild
+      console.assert(node !== null)
+      selectRange(node, 8, node, 31)
+    })
+    cy.get('button:contains("OK")').should('exist')
+    screenshot('multiple-choices-in-wording', 'edit-2', {clearSel: false})
+    cy.get('button:contains("OK")').click()
+    screenshot('multiple-choices-in-wording', 'edit-3')
+
+    cy.get('button:contains("Save then back")').click()
+    visit('/project-xkopqm')
+    cy.get('a:contains("the exported HTML")').should('have.attr', 'href').then(url => {
+      cy.visit(url + '&download=false')
+      cy.get('a').click()
+      screenshot('multiple-choices-in-wording', 'export-1')
+      cy.get('span.main').eq(1).click()
+      screenshot('multiple-choices-in-wording', 'export-2')
+      cy.get('span.choice0').click()
+      screenshot('multiple-choices-in-wording', 'export-3')
     })
   })
 
