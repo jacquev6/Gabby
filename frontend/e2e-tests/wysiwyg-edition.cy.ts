cf55c511a967af029bc77fde130dfa6c60f287ab
Vincent Jacques <vincent@vincent-jacques.net>
2024-10-31 14:09:43 +0100

    Fix end-to-end tests after recent changes

--- frontend/e2e-tests/wysiwyg-edition.cy.ts
+++ frontend/e2e-tests/wysiwyg-edition.cy.ts
@@ -74,7 +74,7 @@ describe('Gabby', () => {
     cy.get('@clueHeader').click()
     cy.get('@clue').type('X')
 
-    for (const adaptationType of ['-', 'select-things', 'fill-with-free-text', 'multiple-choices-in-instructions']) {
+    for (const adaptationType of ['-', 'select-things', 'fill-with-free-text', 'multiple-choices']) {
       cy.get('@adaptationType').select(adaptationType)
 
       for (const fieldAlias of ['@instructions', '@wording', '@example', '@clue']) {
@@ -108,35 +108,6 @@ describe('Gabby', () => {
         cy.get('div:has(>h1:contains("Adapted exercise")) b:contains("bold")').should('not.exist')
       }
     }
-  })
-
-  it('enables the "Choice" button', () => {
-    visit('/project-xkopqm/textbook-klxufv/page-7/new-exercise')
-    setAliases()
-    cy.get('@number').type('test')
-    cy.get('@adaptationType').select('multiple-choices-in-instructions')
-
-    cy.get('button[data-cy="format-choice"]').should('be.disabled')
-
-    cy.get('@instructions').click()
-    cy.get('button[data-cy="format-choice"]').should('be.enabled')
-    cy.get('@instructions').type('plain')
-    cy.get('button[data-cy="format-choice"]').click()
-    cy.get('@instructions').type('choice')
-    cy.get('choice-blot:contains("choice")').should('exist')
-    cy.get('@number').focus()
-    cy.get('button[data-cy="format-choice"]').should('be.disabled')
-
-    cy.get('@wording').click()
-    cy.get('button[data-cy="format-choice"]').should('be.disabled')
-
-    cy.get('@exampleHeader').click()
-    cy.get('@example').should('exist')
-    cy.get('button[data-cy="format-choice"]').should('be.disabled')
-
-    cy.get('@clueHeader').click()
-    cy.get('@clue').should('exist')
-    cy.get('button[data-cy="format-choice"]').should('be.disabled')
   })
 
   it('enables the "Sel" buttons', () => {
@@ -219,32 +190,11 @@ describe('Gabby', () => {
     visit('/project-xkopqm/textbook-klxufv/page-7/exercise-jkrudc', {wysiwyg: false})
 
     cy.get('label:contains("Instructions")').next().type('{selectall}Réponds par {{}choice|vrai} ou {{}choice|faux}.', {delay: 0})
-    cy.get('label:contains("Adaptation type")').next().select('multiple-choices-in-instructions')
+    cy.get('label:contains("Adaptation type")').next().select('multiple-choices')
     notBusy()
 
     cy.get('span:contains("WYSIWYG") input').check()
 
     cy.get(':has(>label:contains("Instructions")) .ql-editor').should('contain.text', 'Réponds par vrai ou faux.')
-  })
-
-  it('creates an exercise with a WYSIWYG field', () => {
-    visit('/project-xkopqm/textbook-klxufv/page-7/new-exercise')
-
-    cy.get('label:contains("Number")').next().type('6')
-    cy.get('label:contains("Adaptation type")').next().select('multiple-choices-in-instructions')
-    cy.get('label:contains("Instructions") + .ql-container .ql-editor').as('instructions')
-    cy.get('@instructions').click().type('Choix : ')
-    cy.get('button[data-cy="format-choice"]').click()
-    cy.get('@instructions').type('vrai')
-    cy.get('button[data-cy="format-choice"]').click()
-    cy.get('@instructions').type(' ou ')
-    cy.get('button[data-cy="format-choice"]').click()
-    cy.get('@instructions').type('faux')
-
-    cy.get('choice-blot').should('have.length', 2)
-
-    cy.get('button:contains("Save then back to list")').click()
-
-    cy.get('li:contains("Choix : {choice|vrai} ou")').should('exist')
   })
 })
