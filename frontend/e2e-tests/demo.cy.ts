78ffaa9f43c99b88f58387af92ad155cb7069a01
Vincent Jacques <vincent@vincent-jacques.net>
2025-01-29 16:13:10 +0100

    Add demo for "QCM beside each sentence" (#66) before implementing #67

--- frontend/e2e-tests/demo.cy.ts
+++ frontend/e2e-tests/demo.cy.ts
@@ -705,4 +705,50 @@ describe('Gabby', () => {
       screenshot('multiple-choices-beside-each-word', 'export-3')
     })
   })
+
+  it('creates a "Multiple choices" exercise with fields beside each sentence', () => {
+    cy.viewport(1300, 1300)
+    visit('/project-xkopqm/textbook-klxufv/page-13/new-exercise', {pdf: 'demo'})
+    setupAliases()
+
+    cy.get('@number').type('1')
+
+    cy.get('@adaptationType').select('multiple-choices')
+
+    traceRectangle('@canvas', 8, 5, 75, 9)
+    cy.get('button:contains("Instructions")').click()
+    notBusy()
+    traceRectangle('@canvas', 7, 9.25, 85, 12)
+    cy.get('button:contains("Wording")').click()
+    notBusy()
+
+    cy.get('button:contains("Multiple choices")').click()
+    cy.get('@instructions').find('p').then($el => {
+      const node = $el[0].firstChild
+      console.assert(node !== null)
+      selectRange(node, 33, node, 73)
+    })
+    cy.get('button:contains("OK")').should('exist')
+    cy.get('label:contains("Start") + input').should('have.value', '')
+    cy.get('label:contains("Stop") + input').should('have.value', '')
+    cy.get('label:contains("Separators") + input').should('have.value', ',')
+    cy.get('label + input').eq(3).should('have.value', 'ou')  // Very fragile selector; sorry, future me!
+    cy.get('label:contains("Placeholder") + input').should('have.value', '')
+    cy.get('button:contains("OK")').click()
+    cy.get('div:contains("Beside each item") > input').click()
+    cy.get('div:contains("Sentences") > input').click()
+    screenshot('multiple-choices-beside-each-sentence', 'edit-1')
+
+    cy.get('button:contains("Save then back")').click()
+    visit('/project-xkopqm')
+    cy.get('a:contains("the exported HTML")').should('have.attr', 'href').then(url => {
+      cy.visit(url + '&download=false')
+      cy.get('a').click()
+      screenshot('multiple-choices-beside-each-sentence', 'export-1')
+      cy.get('span.main').eq(1).click()
+      screenshot('multiple-choices-beside-each-sentence', 'export-2')
+      cy.get('span.choice0').click()
+      screenshot('multiple-choices-beside-each-sentence', 'export-3')
+    })
+  })
 })
