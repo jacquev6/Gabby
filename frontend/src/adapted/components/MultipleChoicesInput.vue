jacquev6/Gabby

commit 784fdb0e17d5f11a09d7e830244f0089fabf8f3b

Alternate colors in stacked lines (#103)

frontend/src/adapted/components/MultipleChoicesInput.vue
@@ -74,7 +74,7 @@ const backdropCovers = inject<string>('adaptedExerciseBackdropCovers', 'body')
     <template v-if="showArrowBefore">→</template>
     <span ref="reference" class="main" :class="{open: showChoices}" @click="showChoices = !showChoices">
       <template v-if="model !== undefined">
-        <Renderable v-for="node in choices[model]" :node :inStack="true" v-model="unusedModels" :modelKey="[0]" />
+        <Renderable v-for="node in choices[model]" :node :nested="true" v-model="unusedModels" :modelKey="[0]" />
       </template>
       <template v-else>
         {{ placeholder }}
@@ -98,7 +98,7 @@ const backdropCovers = inject<string>('adaptedExerciseBackdropCovers', 'body')
           <p v-for="choicesLine in choicesLines" class="choicesLine">
             <template v-for="(choice, i) in choicesLine">
               <span v-if="i !== 0">&nbsp;&nbsp;</span><span class="choice" :class="`choice${choice.colorIndex}`"@click="set(choice.index)">
-                <Renderable v-for="node in choice.content" :node :inStack="true" v-model="unusedModels" :modelKey="[0]" />
+                <Renderable v-for="node in choice.content" :node :nested="true" v-model="unusedModels" :modelKey="[0]" />
               </span>
             </template>
           </p>

commit 7d01926d881f898a91a72b657c859c4495b195cf

Fix coloring issue (introduced in 784fdb0e) (#103)

frontend/src/adapted/components/MultipleChoicesInput.vue
@@ -70,15 +70,17 @@ const backdropCovers = inject<string>('adaptedExerciseBackdropCovers', 'body')
 </script>
 
 <template>
-  <span v-bind="$attrs" style="display: inline flow-root; vertical-align: top">
-    <template v-if="showArrowBefore">→</template>
-    <span ref="reference" class="main" :class="{open: showChoices}" @click="showChoices = !showChoices">
-      <template v-if="model !== undefined">
-        <Renderable v-for="node in choices[model]" :node :nested="true" v-model="unusedModels" :modelKey="[0]" />
-      </template>
-      <template v-else>
-        {{ placeholder }}
-      </template>
+  <span style="display: inline flow-root; vertical-align: top">
+    <span v-bind="$attrs">
+      <template v-if="showArrowBefore">→</template>
+      <span ref="reference" class="main" :class="{open: showChoices}" @click="showChoices = !showChoices">
+        <template v-if="model !== undefined">
+          <Renderable v-for="node in choices[model]" :node :nested="true" v-model="unusedModels" :modelKey="[0]" />
+        </template>
+        <template v-else>
+          {{ placeholder }}
+        </template>
+      </span>
     </span>
     <!-- Insert hidden nodes in the DOM to ensure the floating choices does not cover any text. -->
     <span class="choices" style="display: block; margin-top: -24px; max-width: 0; overflow: hidden; visibility: hidden;">